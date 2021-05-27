d59cd21fdffa6e92f139e89f84fcdfc9ed742321
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-05-27 15:42:34 +0200

    update module 1 assembly

--- src/modules/mudoger-module-1-assembly.sh
+++ src/modules/mudoger-module-1-assembly.sh
@@ -1,1 +1,22 @@
 
+################### 3 ASSEMBLY  #########################################################
+## load conda metawrap conda quality control
+conda activate metawrap-env
+
+assembly_folder="$output_master"/ASSEMBLY
+mkdir -p "$assembly_folder"      # create assembly output folder
+
+# arguments declaration
+log="log_assembly"                # definition of path to assembly log       
+forward_library = $1              # forward library path
+reverse_library = $2              # reverse library path
+output_folder = $3                # output folder to be created inside master output folder
+num_cores = 1                     # number of threads
+assembler="--metaspades"          # --metaspades or --megahit
+# assembly command
+metawrap assembly -1 $forward_library -2 $reverse_library -m $mem_gb -t "$num_cores" "$assembler" -o "$assembly_folder"/"$output_folder"
+
+if [ -f "$assembly_folder"/"$output_folder"/final_assembly.fa ]; 
+then echo  "assembly was succesful" ; 
+else echo "assembly didnt work. please check your resources"; 
+fi
