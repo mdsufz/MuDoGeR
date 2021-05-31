c66a26e7da1f1cb97962cf6d0094208737971970
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-05-31 15:26:43 +0200

    update

--- src/modules/mudoger-module-1.sh
+++ src/modules/mudoger-module-1.sh
@@ -3,18 +3,20 @@
 # help message
 
 
-forward_library = $1              # forward library path
-reverse_library = $2              # reverse library path
-output_folder = $3                # output folder to be created inside master output folder
+forward_library = $1              # forward library path /path/to/libname_1.fastq
+reverse_library = $2              # reverse library path. /path/to/libname_2.fastq
+output_folder = $3                # master output folder to be created and defined by user
 
 
+out_master="$( echo "$output_folder"/"$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_' )")"          # create output master
 
-
+mkdir -p $out_master
 
 # 1 QUALITY CONTROL (QC) OF READS
-/MuDoGeR/src/scripts/mudoger-module-1-1_QC.sh $forward_library $reverse_library $output_folder
+/MuDoGeR/src/scripts/mudoger-module-1-1_QC.sh $forward_library $reverse_library "$out_master"/QC
 
 # 2 KMER COUNT AND MEMORY ESTIMATION FOR ASSEMBLY
-/MuDoGeR/src/scripts/mudoger-module-1-2_kmermempred.sh $forward_library $reverse_library $output_folder
+/MuDoGeR/src/scripts/mudoger-module-1-2_kmermempred.sh $forward_library $reverse_library "$out_master"/kmer
 
 # 3 ASSEMBLY
+/MuDoGeR/src/scripts/mudoger-module-1-assembly.sh $forward_library $reverse_library "$out_master"/assembly
