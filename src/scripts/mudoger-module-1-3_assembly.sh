4dffb8b71d78a333e8cc43210b83dcc41a4bc0e4
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-05-31 15:58:30 +0200

    update

--- src/scripts/mudoger-module-1-3_assembly.sh
+++ src/scripts/mudoger-module-1-3_assembly.sh
@@ -3,8 +3,6 @@
 ## load conda metawrap conda quality control
 conda activate metawrap-env
 
-assembly_folder="$output_master"/ASSEMBLY
-mkdir -p "$assembly_folder"      # create assembly output folder
 
 # arguments declaration
 log="log_assembly"                # definition of path to assembly log       
@@ -13,8 +11,21 @@ reverse_library = $2              # reverse library path
 output_folder = $3                # output folder to be created inside master output folder
 num_cores = 1                     # number of threads
 assembler="--metaspades"          # --metaspades or --megahit
+
+out_master="$( echo "$output_folder"/"$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_' )")"          # create output master
+
+assembly_folder="$out_master"/assembly
+mkdir -p "$assembly_folder"      # create assembly output folder
+
+kmer_file="$out_master"/kmer/final_prediction.tsv
+mem_mb="$(grep "$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_')" $kmer_file | cut -f2)"
+ 
+if [ -z $mem_mb ]; 
+then mem_gb=100;
+else  mem_gb="$(echo $((mem_mb / 1000)))";
+fi
 # assembly command
-metawrap assembly -1 $forward_library -2 $reverse_library -m $mem_gb -t "$num_cores" "$assembler" -o "$assembly_folder"/"$output_folder"
+metawrap assembly -1 $forward_library -2 $reverse_library -m $mem_gb -t "$num_cores" "$assembler" -o "$assembly_folder"
 
 if [ -f "$assembly_folder"/"$output_folder"/final_assembly.fa ]; 
 then echo  "assembly was succesful" ; 
