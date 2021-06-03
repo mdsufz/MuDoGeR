7b5a69dc3dfbc2ead00667e6b3d4bad93a18cf82
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-06-03 15:31:06 +0200

    Update mudoger-module-2-1_initial-binning.sh

--- src/scripts/mudoger-module-2-1_initial-binning.sh
+++ src/scripts/mudoger-module-2-1_initial-binning.sh
@@ -1,1 +1,23 @@
+########## 1 INITIAL PROKARYOTIC BINNING  ###################
+## load conda metawrap conda quality control
+conda activate metawrap-env
 
+# arguments declaration
+log="log_qc"                      # definition of path to QC log       
+assembly=$1
+forward_library = $2              # forward library path
+reverse_library = $3              # reverse library path
+output_folder = $4                # output folder to be created inside master output folder
+num_cores = 1                     # number of threads
+
+
+mkdir $prok/bact_ref
+outb="$prok/bact_ref"
+mkdir $prok/arch_ref
+outa="$prok/arch_ref"
+
+con="$prok/concoct_bins"
+met="$prok/metabat2_bins"
+max="$prok/maxbin2_bins"
+
+metawrap  binning -o $bin -t ${NSLOTS:-1} -a $assembly --run-checkm --metabat2 --maxbin2 --concoct  $forward_library $reverse_library
