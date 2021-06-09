b102a7100f2be30f657a8b9faeb462f1151e73bc
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-06-09 15:32:21 +0200

    Update and rename predict.R to mpred_predict.R

--- tools/mpred_predict.R
+++ tools/mpred_predict.R
@@ -1,9 +1,9 @@
 library(base)
-load("models.RData")
+load("mpred_models.RData")
 
 args = commandArgs(trailingOnly=TRUE)
 if (! file.exists(args[1])) {
-    write("Usage: predict.R <unique_kmer33_55.txt>", stderr())
+    write("Usage: mpred_predict.R <unique_kmer33_55.txt>", stderr())
     quit(status=1)
 }
 
@@ -12,7 +12,7 @@ file_unique_kmers = args[1]
 data <- read.csv(file_unique_kmers, header = F, sep = "\t")
 colnames(data) <- c("dataset","33","55")
 
-source("function_predict_memory.R")
+source("mpred_function_predict_memory.R")
 
 #data <- read.delim(file = "unique_kmer33_55.txt", sep="\t", header=F)
 #data <- read.csv("unique_kmer33_55_EXAMPLE.txt", header = F, sep = "\t")
