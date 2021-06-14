b605c1e1f94ca7ac6f173c8d82d5f7342044fc0e
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-06-14 14:54:17 +0200

    Update mudoger-module-2-5_bin-taxonomy.sh

--- src/scripts/mudoger-module-2-5_bin-taxonomy.sh
+++ src/scripts/mudoger-module-2-5_bin-taxonomy.sh
@@ -4,13 +4,13 @@
 # download database
 # wget https://data.ace.uq.edu.au/public/gtdb/data/releases/release95/95.0/auxillary_files/gtdbtk_r95_data.tar.gz
 # conda or virtualenv activation
-
-source /gpfs1/data/msb/tools/GTDB/gtdbtk-v1.3.0/bin/activate
+conda activate gtdbtk-env
+#source /gpfs1/data/msb/tools/GTDB/gtdbtk-v1.3.0/bin/activate
 
 # necessary path variable
 export GTDBTK_DATA_PATH=/path/to/release95
 
-
 gtdbtk  classify_wf --extension  fa  --cpus ${NSLOTS:-1} --genome_dir "$1"/binning/unique_bins --out_dir "$1"/taxonomy
 
 
+conda deactivate
