ef7c0de27cae06bee7546f5f346191b97ee7ec88
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-06-14 15:46:03 +0200

    Update mudoger-module-2-2_bin-ref-bacteria.sh

--- src/scripts/mudoger-module-2-2_bin-ref-bacteria.sh
+++ src/scripts/mudoger-module-2-2_bin-ref-bacteria.sh
@@ -1,25 +1,21 @@
 ########## 2 PROKARYOTIC REFINEMENT FOR BACTERIA (50,10  ###################
 ## load conda metawrap conda quality control
 conda activate metawrap-env
-conda activate metawrap-env
+
+
 
 # arguments declaration
 log="log_qc"                      # definition of path to QC log       
-assembly=$1
-forward_library = $2              # forward library path
-reverse_library = $3              # reverse library path
-output_folder = $4                # output folder to be created inside master output folder
-num_cores = 1                     # number of threads
+
+output_folder = $1                # output folder to be created inside master output folder
+cores=$2
+assembly=$3
+con=$4
+met=$5
+max=$6
 
 
-mkdir $prok/bact_ref
-outb="$prok/bact_ref"
-mkdir $prok/arch_ref
-outa="$prok/arch_ref"
+metawrap bin_refinement -o "$output_folder" -t $cores -A "$con" -B "$met" -C "$max" -c 50 -x 10
 
-con="$prok/concoct_bins"
-met="$prok/metabat2_bins"
-max="$prok/maxbin2_bins"
+conda deactivate
 
-
-metawrap bin_refinement -o "$outb" -t $num_cores -A "$con" -B "$met" -C "$max" -c 50 -x 10
