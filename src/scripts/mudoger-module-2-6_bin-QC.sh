3f978070ecbe9dd5c00a20915ca90659244cc69b
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-07-05 15:09:14 +0200

    Update mudoger-module-2-6_bin-QC.sh

--- src/scripts/mudoger-module-2-6_bin-QC.sh
+++ src/scripts/mudoger-module-2-6_bin-QC.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 
-extension="fa"
-cores="$1"
-input_bins_folder="$2"/binning/unique_bins
-output_results="$2"/metrics/checkm_qc
+cores="$2"
+input_bins_folder="$1"/binning/unique_bins
+output_results="$1"/metrics/checkm_qc
 
 # loading conda environment
 conda activate metawrap-env
 
+source Mudoger/installation/config 
+
+extension="fa"
+checkm data setRoot "$CHECKM_DB"
 
 # necessary path variable
 checkm lineage_wf -t $cores  --reduced_tree --tab_table -x $extension -f "$1"/metrics/checkm_qc/outputcheckm.tsv $input_bins_folder $output_results
