1ae1726e3220ecd9516bdbf0bff09a726a4ec48f
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-07-05 16:09:43 +0200

    Update mudoger-module-1-1_QC.sh

--- src/scripts/mudoger-module-1-1_QC.sh
+++ src/scripts/mudoger-module-1-1_QC.sh
@@ -13,11 +13,11 @@ conda activate metawrap-env
 
 # arguments declaration
 log="log_qc"                      # definition of path to QC log       
-forward_library = $1              # forward library path
-reverse_library = $2              # reverse library path
-output_folder = $3                # output folder to be created inside master output folder
+forward_library=$1              # forward library path
+reverse_library=$2              # reverse library path
+output_folder=$3                # output folder to be created inside master output folder
 bm_tag="--skip-bmtagger"          # define if human reads should be filtered out or not
-num_cores = 1                     # number of threads
+num_cores=1                     # number of threads
 
 
 out_qc="$( echo "$output_folder"/"$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_' )")/qc"          # create output master
