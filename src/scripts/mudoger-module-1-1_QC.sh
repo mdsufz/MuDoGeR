f25cf9cf4544782363e33ad75f3f93b83a81401a
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2021-07-06 09:58:30 -0300

    Update mudoger-module-1-1_QC.sh

--- src/scripts/mudoger-module-1-1_QC.sh
+++ src/scripts/mudoger-module-1-1_QC.sh
@@ -15,17 +15,17 @@ conda activate metawrap-env
 log="log_qc"                      # definition of path to QC log       
 forward_library=$1              # forward library path
 reverse_library=$2              # reverse library path
-output_folder=$3                # output folder to be created inside master output folder
+qc_folder=$3                # output folder to be created inside master output folder
 bm_tag="--skip-bmtagger"          # define if human reads should be filtered out or not
 num_cores=$4                     # number of threads
 
 
-out_qc="$( echo "$output_folder"/"$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_' )")/qc"          # create output master
+#out_qc="$( echo "$output_folder"/"$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_' )")/qc"          # create output master
 
 #mkdir -p $out_qc
 
 # command of quality control
-metawrap read_qc "$bm_tag" -1 "$forward_library" -2 "$reverse_library" -t "$num_cores" -o "$out_qc"  &> "$out_qc/$log"
+metawrap read_qc "$bm_tag" -1 "$forward_library" -2 "$reverse_library" -t "$num_cores" -o "$qc_folder"  &> "$out_qc"/"$log"
 
 # leave conda environment
 conda deactivate
