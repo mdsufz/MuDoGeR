2985e3e9823cdc26d2c1a6d5e84353cc0750cc9f
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2021-07-06 10:16:06 -0300

    Update mudoger-module-1-3_assembly.sh

--- src/scripts/mudoger-module-1-3_assembly.sh
+++ src/scripts/mudoger-module-1-3_assembly.sh
@@ -6,18 +6,18 @@ conda activate metawrap-env
 
 # arguments declaration
 log="log_assembly"                # definition of path to assembly log       
-forward_library=$1              # forward library path
-reverse_library=$2              # reverse library path
-output_folder=$3                # output folder to be created inside master output folder
-num_cores=1                     # number of threads
+forward_library=$1                # forward library path
+reverse_library=$2                # reverse library path
+master_folder=$3                # output folder to be created inside master output folder
+num_cores=$4                      # number of threads
 assembler="--metaspades"          # --metaspades or --megahit
 
-out_master="$( echo "$output_folder"/"$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_' )")"          # create output master
+#out_master="$( echo "$output_folder"/"$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_' )")"          # create output master
+assembly_folder="$master_folder"/assembly
 
-assembly_folder="$out_master"/assembly
 mkdir -p "$assembly_folder"      # create assembly output folder
 
-kmer_file="$out_master"/kmer/final_prediction.tsv
+kmer_file="$master_folder"/khmer/final_prediction.tsv
 mem_mb="$(grep "$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_')" $kmer_file | cut -f2)"
  
 if [ -z $mem_mb ]; 
@@ -27,7 +27,7 @@ fi
 # assembly command
 metawrap assembly -1 $forward_library -2 $reverse_library -m $mem_gb -t "$num_cores" "$assembler" -o "$assembly_folder"
 
-if [ -f "$assembly_folder"/"$output_folder"/final_assembly.fa ]; 
+if [ -f "$assembly_folder"/final_assembly.fasta ]; 
 then echo  "assembly was succesful" ; 
 else echo "assembly didnt work. please check your resources"; 
 fi
