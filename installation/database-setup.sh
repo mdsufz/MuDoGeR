70d3ebd62a597114d8d54416c233af01c890940a
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-07-07 15:56:55 +0200

    Update database-setup.sh

--- installation/database-setup.sh
+++ installation/database-setup.sh
@@ -4,10 +4,12 @@
 # given the user input, the config file will be edited and all databases via wget, curl, etc and zipping
 
 database_location="$1"
+config_file = MuDoGeR/installation/config
+
 
 mkdir "$database_location"
 
-source MuDoGeR/installation/config
+source "$config_file"
 ############################################### PROKARYOTES ###############################################
 ### CheckM
 mkdir -p "$database_location"/checkm
@@ -19,10 +21,9 @@ rm -f checkm_data_2015_01_16.tar.gz
 # On newer versions of CheckM, you would run:
 #checkm data setRoot /path/to/your/dir/$MY_CHECKM_FOLDER
 CHECKM_DB="$database_location"/"checkm"/checkm_data_2015_01_16
-echo  CHECKM_DB="$CHECKM_DB" >> config
+echo  CHECKM_DB="$CHECKM_DB" >> "$config_file"
 else echo "-> your CheckM database is ready"
 fi
-
 
 ### GTDB-tk
 mkdir -p  "$database_location"/"gtdbtk"
@@ -31,13 +32,19 @@ if [ ! -d gtdbtk_r95_data ]; then
 wget https://data.gtdb.ecogenomic.org/releases/latest/auxillary_files/gtdbtk_data.tar.gz
 tar xvzf gtdbtk_r95_data.tar.gz
 rm -f gtdbtk_r95_data.tar.gz
-echo  GTDBTK_DATA_PATH="$database_location"/gtdbtk/gtdbtk_r95_data >> config
+echo  GTDBTK_DATA_PATH="$database_location"/gtdbtk/gtdbtk_r95_data >> "$config_file"
 else echo "-> your GTDBtk database is ready"
 fi
 
 ############################################### VIRUSES ###############################################
 ### CheckV
 mkdir -p  "$database_location"/checkv
-cd "$database_location"/"gtdbtk"
+cd "$database_location"/checkv
+if [ ! -d checkv-db-v1.0]; then
 wget https://portal.nersc.gov/CheckV/checkv-db-v1.0.tar.gz
 tar -zxvf checkv-db-v1.0.tar.gz
+rm -r checkv-db-v1.0.tar.gz
+else echo "-> your CheckV database is ready"
+fi
+
+
