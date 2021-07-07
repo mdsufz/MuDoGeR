40516368c66d746c1e65d645f4c8d8d45afecee2
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-07-07 15:02:16 +0200

    Update mudoger-module-3-4_vcheck.sh

--- src/scripts/mudoger-module-3-4_vcheck.sh
+++ src/scripts/mudoger-module-3-4_vcheck.sh
@@ -1,1 +1,19 @@
 
+
+
+
+module load Anaconda3/5.3.0
+source activate /data/msb/tools/vcheck/conda-vcheck-env
+export CHECKVDB=/data/msb/tools/vcheck/checkv-db-v0.6
+
+
+
+task=""
+
+date
+echo "starting "$task
+
+
+
+#command
+checkv end_to_end  $1 $2 -t 1
