a14ce431ac7e96e0f2a8dfbb2d3a3968404ff3eb
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2021-07-07 09:41:35 -0300

    Update mudoger-module-2-1_initial-binning.sh

--- src/scripts/mudoger-module-2-1_initial-binning.sh
+++ src/scripts/mudoger-module-2-1_initial-binning.sh
@@ -2,13 +2,12 @@
 ## load conda metawrap conda quality control
 conda activate metawrap-env
 
-# arguments declaration
-log="log_qc"                      # definition of path to QC log       
-assembly=$1
-forward_library = $2              # forward library path
-reverse_library = $3              # reverse library path
-output_folder = $4                # output folder to be created inside master output folder
-num_cores = 1                     # number of threads
+# arguments declaration    
+assembly=$1                     # assembly fasta file
+forward_library=$2              # forward library path
+reverse_library=$3              # reverse library path
+output_folder=$4                # output folder to be created inside master output folder
+num_cores=$5                    # number of threads
 
 
 #mkdir $prok/bact_ref
@@ -20,4 +19,4 @@ num_cores = 1                     # number of threads
 #met="$prok/metabat2_bins"
 #max="$prok/maxbin2_bins"
 
-metawrap  binning -o "$output_folder" -t "$num_cores" -a "$assembly" --run-checkm --metabat2 --maxbin2 --concoct  "$forward_library" "$reverse_library"
+metawrap binning -o "$output_folder" -t "$num_cores" -a "$assembly" --metabat2 --maxbin2 --concoct "$forward_library" "$reverse_library"
