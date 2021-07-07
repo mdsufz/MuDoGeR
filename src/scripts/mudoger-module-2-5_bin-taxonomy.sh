995e7c7749e6b3279cb9f988b6f870dd39618788
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2021-07-07 09:49:28 -0300

    Update mudoger-module-2-5_bin-taxonomy.sh

--- src/scripts/mudoger-module-2-5_bin-taxonomy.sh
+++ src/scripts/mudoger-module-2-5_bin-taxonomy.sh
@@ -6,9 +6,12 @@ conda activate gtdbtk-env
 
 source MuDoGeR/installation/config
 
+master_folder=$1
+cores=$2
+
 # necessary path variable
 #export GTDBTK_DATA_PATH="$gtdbtk_database"
 
-gtdbtk  classify_wf --extension  fa  --cpus "$2" -genome_dir "$1"/binning/unique_bins --out_dir "$1"/metrics/GTDBtk_taxonomy
+gtdbtk  classify_wf --extension  fa  --cpus "$cores" -genome_dir "$master_folder"/binning/unique_bins --out_dir "$master_folder"/metrics/GTDBtk_taxonomy
 
 conda deactivate
