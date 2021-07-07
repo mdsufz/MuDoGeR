7f21b8b9d4acc3bd75a441b28cff8ae95a1b154e
MarcosFlemingBicalho <53937364+MarcosFlemingBicalho@users.noreply.github.com>
2021-07-07 16:44:53 +0200

    Update mudoger-module-1-3_assembly.sh

--- src/scripts/mudoger-module-1-3_assembly.sh
+++ src/scripts/mudoger-module-1-3_assembly.sh
@@ -5,12 +5,13 @@ conda activate metawrap-env
 
 
 # arguments declaration
-log="log_assembly"                # definition of path to assembly log       
-forward_library=$1                # forward library path
-reverse_library=$2                # reverse library path
-master_folder=$3                # output folder to be created inside master output folder
-num_cores=$4                      # number of threads
-assembler="--metaspades"          # --metaspades or --megahit
+log="log_assembly"             # definition of path to assembly log       
+forward_library=$1             # forward library path
+reverse_library=$2             # reverse library path
+master_folder=$3               # output folder to be created inside master output folder
+num_cores=$4                   # number of threads
+assembler=$5                   # --metaspades or --megahit
+
 
 #out_master="$( echo "$output_folder"/"$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_' )")"          # create output master
 assembly_folder="$master_folder"/assembly
@@ -19,13 +20,13 @@ mkdir -p "$assembly_folder"      # create assembly output folder
 
 kmer_file="$master_folder"/khmer/final_prediction.tsv
 mem_mb="$(grep "$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_')" $kmer_file | cut -f2)"
- 
+
 if [ -z $mem_mb ]; 
 then mem_gb=100;
 else  mem_gb="$(echo $((mem_mb / 1000)))";
 fi
 # assembly command
-metawrap assembly -1 $forward_library -2 $reverse_library -m $mem_gb -t "$num_cores" "$assembler" -o "$assembly_folder"
+metawrap assembly -1 $forward_library -2 $reverse_library -m $mem_gb -t "$num_cores" -o "$assembly_folder" "$assembler"
 
 if [ -f "$assembly_folder"/final_assembly.fasta ]; 
 then echo  "assembly was succesful" ; 
