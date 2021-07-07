a767d87b4ce44269a44da6b922b736689b8e12f0
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2021-07-07 09:43:09 -0300

    Update mudoger-module-2-2_bin-ref-bacteria.sh

--- src/scripts/mudoger-module-2-2_bin-ref-bacteria.sh
+++ src/scripts/mudoger-module-2-2_bin-ref-bacteria.sh
@@ -5,14 +5,14 @@ conda activate metawrap-env
 
 
 # arguments declaration
-log="log_qc"                      # definition of path to QC log       
+#log="log_qc"                      # definition of path to QC log       
 
-output_folder = $1                # output folder to be created inside master output folder
+output_folder=$1                # output folder to be created inside master output folder
 cores=$2
 assembly=$3
 con=$4
-met=$5
-max=$6
+max=$5
+met=$6
 
 
 metawrap bin_refinement -o "$output_folder" -t $cores -A "$con" -B "$met" -C "$max" -c 50 -x 10
