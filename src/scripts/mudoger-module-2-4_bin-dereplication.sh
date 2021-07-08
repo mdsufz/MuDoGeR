0e24c71a1e6aac1099c20c0b24d5fa2a3b26898a
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2021-07-08 09:29:37 -0300

    Update mudoger-module-2-4_bin-dereplication.sh

--- src/scripts/mudoger-module-2-4_bin-dereplication.sh
+++ src/scripts/mudoger-module-2-4_bin-dereplication.sh
@@ -5,14 +5,14 @@
 lib=$1;
 bin_count=0; 
 
-md5sum refinement-bac/metawrap*bins/*fa  >  unique_bins/md5_sum ;
-md5sum refinement-arc/metawrap*bins/*fa  >> unique_bins/md5_sum ;
-cat unique_bins/md5_sum | cut -f1 -d' ' | sort | uniq > unique_bins/md5_unique; 
+md5sum "$1"/prokaryotes/binning/refinement-bac/metawrap*bins/*fa  >  "$1"/prokaryotes/binning/unique_bins/md5_sum ;
+md5sum "$1"/prokaryotes/binning/refinement-arc/metawrap*bins/*fa  >> "$1"/prokaryotes/binning/unique_bins/md5_sum ;
+cat "$1"/prokaryotes/binning/unique_bins/md5_sum | cut -f1 -d' ' | sort | uniq > "$1"/prokaryotes/binning/unique_bins/md5_unique; 
 
 while read l; 
 do 
-bininit="$(grep "$l" unique_bins/md5_sum  | head -n1 | cut -f3 -d' ')";
-binafter=unique_bins/"$lib"-bin."$bin_count".fa; 
+bininit="$(grep "$l" "$1"/prokaryotes/binning/unique_bins/md5_sum  | head -n1 | cut -f3 -d' ')";
+binafter="$1"/prokaryotes/binning/unique_bins/"$lib"-bin."$bin_count".fa; 
 cp $bininit $binafter ;
 bin_count=$[$bin_count+1];
-done < unique_bins/md5_unique ; 
+done < "$1"/prokaryotes/binning/unique_bins/md5_unique ; 
