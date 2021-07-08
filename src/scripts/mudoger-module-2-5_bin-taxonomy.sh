d0f896694f04ccb20804caa1da5744cd2f8f6483
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2021-07-08 11:33:18 -0300

    Update mudoger-module-2-5_bin-taxonomy.sh

--- src/scripts/mudoger-module-2-5_bin-taxonomy.sh
+++ src/scripts/mudoger-module-2-5_bin-taxonomy.sh
@@ -10,7 +10,7 @@ master_folder=$1
 cores=$2
 
 # necessary path variable
-export GTDBTK_DATA_PATH="$gtdbtk_database"
+#export GTDBTK_DATA_PATH="$gtdbtk_database"
 
 gtdbtk classify_wf --extension fa --cpus "$cores" --genome_dir "$master_folder"/binning/unique_bins --out_dir "$master_folder"/metrics/GTDBtk_taxonomy
 
