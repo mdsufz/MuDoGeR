7240a9b8ac5b1fc9b2851e9cf7d6af17eb6145b1
MarcosFlemingBicalho <53937364+MarcosFlemingBicalho@users.noreply.github.com>
2021-07-08 15:02:57 +0200

    Update mudoger-module-1-3_assembly.sh

--- src/scripts/mudoger-module-1-3_assembly.sh
+++ src/scripts/mudoger-module-1-3_assembly.sh
@@ -11,7 +11,7 @@ reverse_library=$2             # reverse library path
 master_folder=$3               # output folder to be created inside master output folder
 num_cores=$4                   # number of threads
 assembler=$5                   # --metaspades or --megahit
-mem_gb=$6                      # add the amount of memory in case you do not want to use the amount predicted
+mem_gb=$6                      # add the amount of memory in case you do not want to use the amount predicted. Give amount in GB. Ex: "10" for 10GB.
 
 #out_master="$( echo "$output_folder"/"$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_' )")"          # create output master
 assembly_folder="$master_folder"/assembly
@@ -21,15 +21,19 @@ mkdir -p "$assembly_folder"      # create assembly output folder
 kmer_file="$master_folder"/khmer/final_prediction.tsv
 #mem_mb="$(grep "$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_')" $kmer_file | cut -f2)"
 
-if [ -z "$6" ]
-then mem_mb="$(grep "$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_')" $kmer_file | cut -f2)"
+if [ -z "$6" ];
+then mem_mb="$(grep "$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_')" $kmer_file | cut -f2)"; mem_gb="$(echo $((mem_mb / 1000)))";
 else mem_gb="$6"
 fi
 
-if [ -z $mem_mb ]; 
+if [ -z "$mem_mb" ]; 
 then mem_gb=100;
-else  mem_gb="$(echo $((mem_mb / 1000)))";
 fi
+
+#if [ -z $mem_mb ]; 
+#then mem_gb=100;
+#else  mem_gb="$(echo $((mem_mb / 1000)))";
+#fi
 
  assembly command
 metawrap assembly -1 $forward_library -2 $reverse_library -m $mem_gb -t "$num_cores" -o "$assembly_folder" "$assembler"
