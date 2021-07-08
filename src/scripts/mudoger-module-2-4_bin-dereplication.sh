f55e85bf2fb27dcd9442dae0f1f6f942d0aeedb6
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2021-07-08 11:52:56 -0300

    added lib_name
    lib_folder was used where lib_name was intended

--- src/scripts/mudoger-module-2-4_bin-dereplication.sh
+++ src/scripts/mudoger-module-2-4_bin-dereplication.sh
@@ -2,17 +2,19 @@
 
 # 4 BIN REDUNANCY REMOVAL
 
-lib=$1;
-bin_count=0; 
+lib_folder=$1;
+bin_count=0;
 
-md5sum "$1"/prokaryotes/binning/refinement-bac/metawrap*bins/*fa  >  "$1"/prokaryotes/binning/unique_bins/md5_sum ;
-md5sum "$1"/prokaryotes/binning/refinement-arc/metawrap*bins/*fa  >> "$1"/prokaryotes/binning/unique_bins/md5_sum ;
-cat "$1"/prokaryotes/binning/unique_bins/md5_sum | cut -f1 -d' ' | sort | uniq > "$1"/prokaryotes/binning/unique_bins/md5_unique; 
+lib_name=$(basename $lib_folder);
+
+md5sum "$lib_folder"/prokaryotes/binning/refinement-bac/metawrap*bins/*fa  >  "$lib_folder"/prokaryotes/binning/unique_bins/md5_sum ;
+md5sum "$lib_folder"/prokaryotes/binning/refinement-arc/metawrap*bins/*fa  >> "$lib_folder"/prokaryotes/binning/unique_bins/md5_sum ;
+cat "$lib_folder"/prokaryotes/binning/unique_bins/md5_sum | cut -f1 -d' ' | sort | uniq > "$lib_folder"/prokaryotes/binning/unique_bins/md5_unique; 
 
 while read l; 
 do 
-bininit="$(grep "$l" "$1"/prokaryotes/binning/unique_bins/md5_sum  | head -n1 | cut -f3 -d' ')";
-binafter="$1"/prokaryotes/binning/unique_bins/"$lib"-bin."$bin_count".fa; 
+bininit="$(grep "$l" "$lib_folder"/prokaryotes/binning/unique_bins/md5_sum  | head -n1 | cut -f3 -d' ')";
+binafter="$lib_folder"/prokaryotes/binning/unique_bins/"$lib_name"-bin."$bin_count".fa; 
 cp $bininit $binafter ;
 bin_count=$[$bin_count+1];
 done < "$1"/prokaryotes/binning/unique_bins/md5_unique ; 
