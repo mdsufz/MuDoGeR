92ab7be9275923d419f07127f9ada2c554917cae
MarcosFlemingBicalho <53937364+MarcosFlemingBicalho@users.noreply.github.com>
2021-07-08 14:45:15 +0200

    Update mudoger-module-1-3_assembly.sh

--- src/scripts/mudoger-module-1-3_assembly.sh
+++ src/scripts/mudoger-module-1-3_assembly.sh
@@ -11,7 +11,7 @@ reverse_library=$2             # reverse library path
 master_folder=$3               # output folder to be created inside master output folder
 num_cores=$4                   # number of threads
 assembler=$5                   # --metaspades or --megahit
-
+mem_gb=$6                      # add the amount of memory in case you do not want to use the amount predicted
 
 #out_master="$( echo "$output_folder"/"$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_' )")"          # create output master
 assembly_folder="$master_folder"/assembly
@@ -21,11 +21,16 @@ mkdir -p "$assembly_folder"      # create assembly output folder
 kmer_file="$master_folder"/khmer/final_prediction.tsv
 mem_mb="$(grep "$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_')" $kmer_file | cut -f2)"
 
+if [ -z "mem_gb" ]
+then mem_gb=$6
+fi
+
 if [ -z $mem_mb ]; 
 then mem_gb=100;
 else  mem_gb="$(echo $((mem_mb / 1000)))";
 fi
-# assembly command
+
+ assembly command
 metawrap assembly -1 $forward_library -2 $reverse_library -m $mem_gb -t "$num_cores" -o "$assembly_folder" "$assembler"
 
 if [ -f "$assembly_folder"/final_assembly.fasta ]; 
