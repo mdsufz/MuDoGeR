f4e9c4428eb063369b88abb825a63e62f119dfca
MarcosFlemingBicalho <53937364+MarcosFlemingBicalho@users.noreply.github.com>
2021-07-21 15:57:36 +0200

    Update mudoger-module-2-3_bin-ref-archea.sh

--- src/scripts/mudoger-module-2-3_bin-ref-archea.sh
+++ src/scripts/mudoger-module-2-3_bin-ref-archea.sh
@@ -13,11 +13,12 @@ assembly=$3
 con=$4
 max=$5
 met=$6
+memory=$7
 
 source Mudoger/installation/config_db
 
 checkm data setRoot "$CHECKM_DB"
 
-metawrap bin_refinement -o "$output_folder" -t $cores -A "$con" -B "$met" -C "$max" -c 40 -x 30
+metawrap bin_refinement -o "$output_folder" -t $cores -A "$con" -B "$met" -C "$max" -c 40 -x 30 -m "$memory"
 
 conda deactivate
