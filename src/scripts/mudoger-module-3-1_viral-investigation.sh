4ca7d0a14f7ae65aec7abca6d22d5fc54ac03255
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-08-09 15:42:17 +0200

    Update mudoger-module-3-1_viral-investigation.sh

--- src/scripts/mudoger-module-3-1_viral-investigation.sh
+++ src/scripts/mudoger-module-3-1_viral-investigation.sh
@@ -17,22 +17,24 @@ python VIBRANT_run.py -i $assembly -folder "$output_folder"/vibrant -t $num_core
 # fetch results
 cat "$output_folder"/vibrant/VIBRANT_final_assembly/VIBRANT_phages_final_assembly/final_assembly.phages_combined.fna | 
 grep ">" | sed "s/_fragment_1//g;s/>//g"   > "$output_folder"/VIBRANT_filtered_data.txt
-
+conda deactivate
 
 ######### VIRFINDER
 conda activate virfinder-env
 mkdir -p "$output_folder"/virfinder
-Rscript /data/msb/tools/virfinder/virfinder_script.r "$output_folder"/virfinder "$assembly" "$output_folder"/virfinder/virfinder_output.tsv
+Rscript MuDoGeR/tools/virfinder_script.r "$output_folder"/virfinder "$assembly" "$output_folder"/virfinder/virfinder_output.tsv
 # fetch results
 cat "$output_folder"/virfinder/virfinder_output.tsv | awk -F'\t' '{ if ( $4 <= 0.01) print }' | 
 awk -F'_' '{ if ( $4 >= 1000) print  }' | cut -f2 | sed "s/\"//g" > "$output_folder"/virfinder/virfinder_filtered_data.txt
+conda deactivate
 
 ######### VIRSORTER
-conda activate virsorter-env
+conda activate virsorter2-env
 virsorter run all -i "$assembly" -w "$output_folder"/virsorter -j "$num_cores"
 # fetch results
-cat "$output_folder"/virsorter2/final-viral-combined.fa  | grep ">" | sed "s/_fragment_1//g;s/>//g" | 
+cat "$output_folder"/virsorter/final-viral-combined.fa  | grep ">" | sed "s/_fragment_1//g;s/>//g" | 
 cut -f1 -d "|" > "$output_folder"/virsorter/virsorter2_filtered_data.txt
+conda deactivate
 
 ####### GET RESULTS AND PUT THEM TOGETHER
 mkdir -p "$output_folder"/dereplication
@@ -40,10 +42,13 @@ cat "$output_folder"/VIBRANT_filtered_data.txt \
 "$output_folder"/virfinder/virfinder_filtered_data.txt \ 
 "$output_folder"/virsorter/virsorter2_filtered_data.txt | sort -u >  "$output_folder"/dereplication/viral_unique_contigs
 
-### EXTRACT 
-python extract_fa.py "$output_folder"/dereplication/viral_unique_contigs "$assembly" "$output_folder"/dereplication/uvigs.fa
+### PICK FASTA SEQUENCES OF VIRAL CONTIGS FOR FURTHER DEREPLICATION
+conda activate extract-env
+python MuDoGeR/tools/extract_fa.py "$output_folder"/dereplication/viral_unique_contigs "$assembly" "$output_folder"/dereplication/uvigs.fa
+conda deactivate
 
 ##### RUN DEREPLICATION
+conda activate stampede-clustergenomes-env
 Cluster_genomes.pl -f "$output_folder"/dereplication/uvigs.fa  -c 70 -i 95
-
+conda deactivate
  
