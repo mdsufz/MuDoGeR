10d918ff8252535450cffc64c046de7d6d146bf5
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-08-09 16:12:54 +0200

    Update mudoger-module-3-2_vcontact2.sh

--- src/scripts/mudoger-module-3-2_vcontact2.sh
+++ src/scripts/mudoger-module-3-2_vcontact2.sh
@@ -4,7 +4,6 @@
 # FIX ENVIRONMENT WITH VCONTACT INSTALLED INSIDE
 # ADJUST PATHS OF TOOLS CALLED IN THE SCRIPT BELOW
 
-conda activate /gpfs1/data/msb/tools/vcontact2/Vontact2
 
 uvigs="$1"
 folder_taxonomy="$2"
@@ -14,6 +13,8 @@ date
 # creates output directory
 mkdir $2
 
+conda activate vcontact2-env
+
 # runs prodigal to generate 1st input to vcontact
 prodigal -i "$uvigs" -o  "$folder_taxonomy"/viral_genomes.genes -a "$folder_taxonomy"/viral_genomes.faa -p meta
 
@@ -21,9 +22,10 @@ prodigal -i "$uvigs" -o  "$folder_taxonomy"/viral_genomes.genes -a "$folder_taxo
 vcontact2_gene2genome -p "$folder_taxonomy"/viral_genomes.faa -o "$folder_taxonomy"/viral_genomes_g2g.csv -s 'Prodigal-FAA'
 
 # runs vcontact
-vcontact -t "$cores" --raw-proteins "$folder_taxonomy"/viral_genomes.faa --rel-mode 'Diamond' --proteins-fp "$folder_taxonomy"/viral_genomes_g2g.csv --db 'ProkaryoticViralRefSeq94-Merged' --pcs-mode MCL --vcs-mode ClusterONE --c1-bin /gpfs1/data/msb/tools/vcontact2/cluster_one-1.0.jar --output-dir "$2"/vcontact-output
+vcontact -t "$cores" --raw-proteins "$folder_taxonomy"/viral_genomes.faa --rel-mode 'Diamond' --proteins-fp "$folder_taxonomy"/viral_genomes_g2g.csv --db 'ProkaryoticViralRefSeq94-Merged' --pcs-mode MCL --vcs-mode ClusterONE --c1-bin cluster_one-1.0.jar --output-dir "$2"/vcontact-output
 
 
 # process output of vcontact (TO BE DEFINED)
 #cat "$folder_taxonomy"/vcontact-output/genome_by_genome_overview.csv  | grep NODE    | cut -f1 -d',' > "$folder_taxonomy"/AUX-1
 #while read l; do   grep "$l" "$2"/genome_by_genome_overview.csv | tr '\' ',' | cut -f1,2,19 -d','  ; done < "$2"/AUX-1 > "$2"/AUX-2
+conda deactivate
