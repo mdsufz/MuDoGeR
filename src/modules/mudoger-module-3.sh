680743bb92c66d997723bdfaf936524824a6fe2c
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2021-08-10 18:34:07 -0300

    Update mudoger-module-3.sh

--- src/modules/mudoger-module-3.sh
+++ src/modules/mudoger-module-3.sh
@@ -15,10 +15,10 @@
 
 
 #libname=$1
-assembly = $1
+assembly=$1
 #forward_library = $3            # forward library path /path/to/libname_1.fastq    # commented because reads are not used
 #reverse_library = $4            # reverse library path. /path/to/libname_2.fastq   # and thus not necessary
-libname_folder = $2              # master output folder to be created and defined by user
+libname_folder=$2              # master output folder to be created and defined by user
 cores=10
 
 # 1 viral investigation
@@ -29,10 +29,12 @@ bash -i MuDoGeR/src/scripts/mudoger-module-3-1-viral_investigation.sh "$assembly
 bash -i MuDoGeR/src/scripts/mudoger-module-3-2_vcontact2.sh "$libname_folder"/viruses/investigation/dereplication/uvigs_95-70.fna "$libname_folder"/viruses/taxonomy "$cores"
 
 # 3 host prediction
-bash -i MuDoGeR/src/scripts/mudoger-module-3-3_host-prediction.sh "$libname_folder"/viruses/investigation/dereplication/uvigs_95-70.fna  "$libname_folder"/prokaryotes/binning/unique_bins "$libname"/viruses/dereplication/host_prediction
+#commented for testing 
+#bash -i MuDoGeR/src/scripts/mudoger-module-3-3_host-prediction.sh "$libname_folder"/viruses/investigation/dereplication/uvigs_95-70.fna  "$libname_folder"/prokaryotes/binning/unique_bins "$libname"/viruses/dereplication/host_prediction
 
 # 4 vcheck
-bash -i MuDoGeR/src/scripts/mudoger-module-3-4_vcheck.sh "$libname_folder"/viruses/dereplication/uvigs/uvigs_95-70.fna "$libname_folder"/viruses/vcheck_quality
+#commented for testing
+#bash -i MuDoGeR/src/scripts/mudoger-module-3-4_vcheck.sh "$libname_folder"/viruses/dereplication/uvigs/uvigs_95-70.fna "$libname_folder"/viruses/vcheck_quality
 
 # 5 uvigs metrics
 # bash -i MuDoGeR/src/scripts/mudoger-module-3-5_uvigs-metrics.sh
