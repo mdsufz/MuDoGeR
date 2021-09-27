e63d33ddfedc8cfaf68fe2f6957cda056ac75419
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2021-09-27 15:06:55 -0300

    Update mudoger-module-2-6_bin-QC.sh

--- src/scripts/mudoger-module-2-6_bin-QC.sh
+++ src/scripts/mudoger-module-2-6_bin-QC.sh
@@ -8,10 +8,12 @@ output_results="$1"/metrics/checkm_qc
 # loading conda environment
 conda activate metawrap-env
 
-source Mudoger/installation/config_db
+source MuDoGeR/installation/config_db
 
 extension="fa"
-checkm data setRoot "$CHECKM_DB"
+
+#Set it only once during database configuration
+#checkm data setRoot "$CHECKM_DB"
 
 # necessary path variable
 checkm lineage_wf -t $cores --reduced_tree --tab_table -x $extension -f "$1"/metrics/checkm_qc/outputcheckm.tsv $input_bins_folder $output_results
