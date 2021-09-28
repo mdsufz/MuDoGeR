b8a2a65e630a7be916ab2b16eaa95e062e73f542
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-09-28 16:52:49 +0200

    Update mudoger-module-3.sh

--- src/modules/mudoger-module-3.sh
+++ src/modules/mudoger-module-3.sh
@@ -14,10 +14,8 @@
 
 
 
-#libname=$1
+
 assembly=$1
-#forward_library = $3            # forward library path /path/to/libname_1.fastq    # commented because reads are not used
-#reverse_library = $4            # reverse library path. /path/to/libname_2.fastq   # and thus not necessary
 libname_folder=$2              # master output folder to be created and defined by user
 cores=10
 
