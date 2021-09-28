bf03345696cf1f6d16bc775877bd8a6ceb2da1bd
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-09-28 16:47:37 +0200

    Update mudoger-module-3.sh

--- src/modules/mudoger-module-3.sh
+++ src/modules/mudoger-module-3.sh
@@ -23,14 +23,14 @@ cores=10
 
 # 1 viral investigation
 mkdir -p "$libname_folder"/viruses/investigation
-bash -i MuDoGeR/src/scripts/mudoger-module-3-1_viral-investigation.sh "$assembly" "$libname_folder"/viruses/investigation $cores
+#bash -i MuDoGeR/src/scripts/mudoger-module-3-1_viral-investigation.sh "$assembly" "$libname_folder"/viruses/investigation $cores
 
 # 2 viral vcontact2 (taxonomy)
 #bash -i MuDoGeR/src/scripts/mudoger-module-3-2_vcontact2.sh "$libname_folder"/viruses/investigation/dereplication/uvigs_95-70.fna "$libname_folder"/viruses/taxonomy "$cores"
 
 # 3 host prediction
 #commented for testing 
-#bash -i MuDoGeR/src/scripts/mudoger-module-3-3_host-prediction.sh "$libname_folder"/viruses/investigation/dereplication/uvigs_95-70.fna  "$libname_folder"/prokaryotes/binning/unique_bins "$libname"/viruses/dereplication/host_prediction
+bash -i MuDoGeR/src/scripts/mudoger-module-3-3_host-prediction.sh "$libname_folder"/viruses/investigation/dereplication/uvigs_95-70.fna  "$libname_folder"/prokaryotes/binning/unique_bins "$libname_folder"/viruses/dereplication/host_prediction
 
 # 4 vcheck
 #commented for testing
