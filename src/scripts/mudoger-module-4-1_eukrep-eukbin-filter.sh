d2bb50c025388b8ee061cc6ddbeb15672421bd7b
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-09-30 17:26:46 +0200

    Create mudoger-module-4-1_eukrep-eukbin-filter.sh

---
+++ src/scripts/mudoger-module-4-1_eukrep-eukbin-filter.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+
+########## 1 INITIAL PROKARYOTIC BINNING  ###################
+## load conda metawrap conda quality control
+conda activate metawrap-env
+
+# arguments declaration    
+assembly=$1                     # assembly fasta file
+forward_library=$2              # forward library path
+reverse_library=$3              # reverse library path
+output_folder=$4                # output folder to be created inside master output folder
+num_cores=$5                    # number of threads
+memory=$6
+
+#mkdir $prok/bact_ref
+#outb="$prok/bact_ref"
+#mkdir $prok/arch_ref
+#outa="$prok/arch_ref"
+
+#con="$prok/concoct_bins"
+#met="$prok/metabat2_bins"
+#max="$prok/maxbin2_bins"
+
+conda activate eukrep-env
+
+EukRep -i "$assembly" --prokarya "$output_folder"/prokaryotic_contigs -o "$output_folder"/eukaryotic_contigs
+
+
+#metawrap binning -o "$output_folder" -t "$num_cores" -a "$output_folder"/eukaryotic_contigs --concoct "$forward_library" "$reverse_library" # -m "$memory" 
