c3bb60eabcf2d6f1cd27495ac72a71aa34ed8bae
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-10-04 10:38:52 +0200

    Update mudoger-module-4-1_eukrep-eukbin-filter.sh

--- src/scripts/mudoger-module-4-1_eukrep-eukbin-filter.sh
+++ src/scripts/mudoger-module-4-1_eukrep-eukbin-filter.sh
@@ -13,14 +13,14 @@ memory=$6
 
 
 # fetch eukaryotic contigs/scaffolds
-conda activate eukrep-env
-EukRep -i "$assembly" --prokarya "$output_folder"/prokaryotic_contigs -o "$output_folder"/eukaryotic_contigs
-conda deactivate
+#conda activate eukrep-env
+#EukRep -i "$assembly" --prokarya "$output_folder"/prokaryotic_contigs -o "$output_folder"/eukaryotic_contigs
+#conda deactivate
 
 
 # run concoct binnning
-conda activate metawrap-env
-metawrap binning -o "$output_folder" -t "$num_cores" -a "$output_folder"/eukaryotic_contigs --concoct "$forward_library" "$reverse_library" # -m "$memory" 
+#conda activate metawrap-env
+#metawrap binning -o "$output_folder" -t "$num_cores" -a "$output_folder"/eukaryotic_contigs --concoct "$forward_library" "$reverse_library" # -m "$memory" 
 
 
 # keep bins bigger than 2,5mb
