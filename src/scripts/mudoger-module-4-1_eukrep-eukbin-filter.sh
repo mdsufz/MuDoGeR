a37cc0b2f690608ba8069bcd7b8dc39c891bc34e
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-10-04 10:25:07 +0200

    Update mudoger-module-4-1_eukrep-eukbin-filter.sh

--- src/scripts/mudoger-module-4-1_eukrep-eukbin-filter.sh
+++ src/scripts/mudoger-module-4-1_eukrep-eukbin-filter.sh
@@ -13,25 +13,20 @@ output_folder=$4                # output folder to be created inside master outp
 num_cores=$5                    # number of threads
 memory=$6
 
-#mkdir $prok/bact_ref
-#outb="$prok/bact_ref"
-#mkdir $prok/arch_ref
-#outa="$prok/arch_ref"
 
-#con="$prok/concoct_bins"
-#met="$prok/metabat2_bins"
-#max="$prok/maxbin2_bins"
-
+# fetch eukaryotic contigs/scaffolds
 conda activate eukrep-env
-
 EukRep -i "$assembly" --prokarya "$output_folder"/prokaryotic_contigs -o "$output_folder"/eukaryotic_contigs
+conda deactivate
 
 
+# run concoct binnning
+conda activate metawrap-env
 metawrap binning -o "$output_folder" -t "$num_cores" -a "$output_folder"/eukaryotic_contigs --concoct "$forward_library" "$reverse_library" # -m "$memory" 
 
-mkdir filtered_bin_size 
 
 # keep bins bigger than 2,5mb
+mkdir filtered_bin_size 
 for bin in concoct_binning/concoct_bins/*fa; 
 do  size="$(stat $bin | grep Size | cut -f1  | cut -f4 -d' ' )"; 
 if [ $size -gt 2500000 ]; then  cp "$bin" filtered_bin_size ; 
