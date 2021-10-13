3bc76b5d7145e2a28d0c93032aa6d7fd49911b1f
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-10-13 11:06:05 +0200

    Update mudoger-module-3-2_vcontact2.sh

--- src/scripts/mudoger-module-3-2_vcontact2.sh
+++ src/scripts/mudoger-module-3-2_vcontact2.sh
@@ -16,16 +16,20 @@ mkdir $2
 conda activate vcontact2-env
 
 # runs prodigal to generate 1st input to vcontact
+echo "-> step 1/4"
 prodigal -i "$uvigs" -o  "$folder_taxonomy"/viral_genomes.genes -a "$folder_taxonomy"/viral_genomes.faa -p meta
 
 # runs auxiliary script to generate 2nd input to vcontact
+echo "-> step 2/4"
 vcontact2_gene2genome -p "$folder_taxonomy"/viral_genomes.faa -o "$folder_taxonomy"/viral_genomes_g2g.csv -s 'Prodigal-FAA'
 
 # runs vcontact
+echo "-> step 3/4"
 vcontact2 -t "$cores" --raw-proteins "$folder_taxonomy"/viral_genomes.faa --rel-mode 'Diamond' --proteins-fp "$folder_taxonomy"/viral_genomes_g2g.csv --db 'ProkaryoticViralRefSeq94-Merged' --pcs-mode MCL --vcs-mode ClusterONE --c1-bin cluster_one-1.0.jar --output-dir "$2"/vcontact-output
 
 
 # process output of vcontact (TO BE DEFINED)
+echo "-> step 4/4"
 #cat "$folder_taxonomy"/vcontact-output/genome_by_genome_overview.csv  | grep NODE    | cut -f1 -d',' > "$folder_taxonomy"/AUX-1
 #while read l; do   grep "$l" "$2"/genome_by_genome_overview.csv | tr '\' ',' | cut -f1,2,19 -d','  ; done < "$2"/AUX-1 > "$2"/AUX-2
 conda deactivate
