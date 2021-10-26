312e81861d4c168c61ce175503429c1b36ee83e8
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-10-26 10:29:30 +0200

    addition of module 0

---
+++ src/modules/mudoger-start.sh
@@ -0,0 +1,39 @@
+
+
+mudoger_lettering(){
+echo -e "\n"
+echo -e "\t███    ███ ██    ██ ██████   ██████   ██████  ███████ ██████  "
+echo -e "\t████  ████ ██    ██ ██   ██ ██    ██ ██       ██      ██   ██ "
+echo -e "\t██ ████ ██ ██    ██ ██   ██ ██    ██ ██   ███ █████   ██████  "
+echo -e "\t██  ██  ██ ██    ██ ██   ██ ██    ██ ██    ██ ██      ██   ██ "
+echo -e "\t██      ██  ██████  ██████   ██████   ██████  ███████ ██   ██ "                                                                                                                    
+echo -e "\t\t\tMulti-Domain Genome Recovery"
+echo -e "\t\t\t\tVersion 1.0.0\n\n"
+}
+  
+
+mudoger_lettering
+
+echo '--> Please provide a path to the metadata file. ($ mudoger -help -md) for explanation'
+
+read metadata
+echo 'path to metadata file: '$metadata
+echo 'reading '$metadata
+
+out="$(python mdcheck.py $metadata)"
+
+echo -e "$out"
+
+### check if there was any problem with metadata file
+if [[ $out =~ "Closing" ]]; then
+   echo -e "\n TIP: run \"mudoger -h\" for help "
+   exit 0
+fi
+
+
+# if everything is okay, proceeds with asking user for output folder
+echo '--> Please provide a path for the output folder. If it does not exist, it will be created'
+read output_folder
+
+mkdir -p "$output_folder"
+echo '--> Folder created'
