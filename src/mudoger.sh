89f892df07e6f34c235b5dac35852a8358747fd2
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-10-26 17:33:51 +0200

    Update mudoger.sh

--- src/mudoger.sh
+++ src/mudoger.sh
@@ -79,8 +79,8 @@ if [ "$active_module" = preprocess ]; then
 	aux="$(while read l ; do echo "$l" | cut -f1; done < "$metadata_table"  | tr '\t' '\n' | sort |  uniq)";
 	for i in $aux; 
 	do 
-	r1="$(cat "$md" | awk -F '\t' '{ if ($1 == "'$i'") {print} }' | cut -f2 | grep '_1.f')"; 
-	r2="$(cat "$md" | awk -F '\t' '{ if ($1 == "'$i'") {print} }' | cut -f2 | grep '_2.f')";
+	r1="$(cat "$metadata_table" | awk -F '\t' '{ if ($1 == "'$i'") {print} }' | cut -f2 | grep '_1.f')"; 
+	r2="$(cat "$metadata_table" | awk -F '\t' '{ if ($1 == "'$i'") {print} }' | cut -f2 | grep '_2.f')";
 	time $module_script -1 $r1 -2 $r2 -o $output_folder/$i -t $num_cores;
 	done
 
