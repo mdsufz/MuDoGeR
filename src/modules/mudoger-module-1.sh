c1d0e80b5b780a130eae5d3cec89991bd8a4a97f
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-10-27 14:47:13 +0200

    Update mudoger-module-1.sh

--- src/modules/mudoger-module-1.sh
+++ src/modules/mudoger-module-1.sh
@@ -57,7 +57,7 @@ else echo "run qc"
 fi
 exit 0
 
-#bash -i MuDoGeR/src/scripts/mudoger-module-1-1_QC.sh "$forward_library" "$reverse_library" "$master_output_dir"/qc "$num_cores"
+bash -i MuDoGeR/src/scripts/mudoger-module-1-1_QC.sh "$forward_library" "$reverse_library" "$master_output_dir"/qc "$num_cores"
 
 # 2 KMER COUNT AND MEMORY ESTIMATION FOR ASSEMBLY
 #bash -i MuDoGeR/src/scripts/mudoger-module-1-2_kmermempred.sh "$master_output_dir"/qc/final_pure_reads_1.fastq "$master_output_dir"/khmer
