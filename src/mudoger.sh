d0d00393f7350dab36028029ddfe10dcb9cb59b8
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-10-28 10:59:09 +0200

    Update mudoger.sh

--- src/mudoger.sh
+++ src/mudoger.sh
@@ -96,7 +96,7 @@ elif [ "$active_module" = prokaryotes ]; then
 	r1="$(cat "$metadata_table" | awk -F '\t' '{ if ($1 == "'$i'") {print} }' | cut -f2 | grep '_1.f')"; 
 	r2="$(cat "$metadata_table" | awk -F '\t' '{ if ($1 == "'$i'") {print} }' | cut -f2 | grep '_2.f')";
 	#ime $module_script -1 $r1 -2 $r2 -o $output_folder/$i -t $num_cores;
-	echo $module_script -1 $r1 -2 $r2 -o $output_folder/$i -t $num_cores;
+	echo $module_script -1 $r1 -2 $r2 -a $output_folder/$i/assembly/final_assembly.fasta -o $output_folder/$i -t $num_cores;
 	#echo 'test'
 	done
 else
