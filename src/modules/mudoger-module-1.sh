00f68e052de618686346a76ae35258a684b08ced
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-10-28 10:39:06 +0200

    Update mudoger-module-1.sh

--- src/modules/mudoger-module-1.sh
+++ src/modules/mudoger-module-1.sh
@@ -53,13 +53,14 @@ mkdir -p "$master_output_dir"
 # 1 QUALITY CONTROL (QC) OF READS
 if [ -f  "$master_output_dir"/qc/final_pure_reads_1.fastq ]; 		# if one of the outputs is already there, do not run
 then echo "Reads were already qc'ed. Please check here: "$master_output_dir"/qc"
-else bash -i MuDoGeR/src/scripts/mudoger-module-1-1_QC.sh "$forward_library" "$reverse_library" "$master_output_dir"/qc "$num_cores"
+else echo "-> Running QC"
+bash -i MuDoGeR/src/scripts/mudoger-module-1-1_QC.sh "$forward_library" "$reverse_library" "$master_output_dir"/qc "$num_cores"
 fi
 
 # 2 KMER COUNT AND MEMORY ESTIMATION FOR ASSEMBLY
 if [ -f  "$master_output_dir"/khmer/metaspades_prediction.tsv ]; 
 then echo "Memory prediction is done. Please check here: "$master_output_dir"/khmer"
-else echo "run khmer pred"
+else echo "-> Running khmer pred"
 bash -i MuDoGeR/src/scripts/mudoger-module-1-2_kmermempred.sh "$master_output_dir"/qc/final_pure_reads_1.fastq "$master_output_dir"/khmer
 fi
 
@@ -67,37 +68,10 @@ fi
 
 # 3 ASSEMBLY
 if [ -f  "$master_output_dir"/assembly/final_assembly.fasta ]; 
-then echo "Assembly is reading. Please check here: "$master_output_dir"/assembly"
-else echo "run assembly"
+then echo "-> Assembly is reading. Please check here: "$master_output_dir"/assembly"
+else echo "-> Running assembly"
 #bash -i MuDoGeR/src/scripts/mudoger-module-1-3_assembly.sh "$master_output_dir"/qc/final_pure_reads_1.fastq "$master_output_dir"/qc/final_pure_reads_2.fastq  "$master_output_dir" "$num_cores" "$metaspades" #"$memory"
 fi
 
+echo "-> module 1 (preprocess and assembly) is finished
 
-
-
-
-
-
-
-# help message
-
-
-#forward_library=$1              # forward library path /path/to/libname_1.fastq
-#reverse_library=$2              # reverse library path. /path/to/libname_2.fastq
-#output_folder=$3                # master output folder to be created and defined by user
-#num_cores=$4                    # number of cores to use
-
-#lib="$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_' )"          # create output master
-
-#master_output_dir="$output_folder"/"$lib"
-
-#mkdir -p "$master_output_dir"
-
-# 1 QUALITY CONTROL (QC) OF READS
-#bash -i MuDoGeR/src/scripts/mudoger-module-1-1_QC.sh "$forward_library" "$reverse_library" "$master_output_dir"/qc "$num_cores"
-
-# 2 KMER COUNT AND MEMORY ESTIMATION FOR ASSEMBLY
-#bash -i MuDoGeR/src/scripts/mudoger-module-1-2_kmermempred.sh "$master_output_dir"/qc/final_pure_reads_1.fastq "$master_output_dir"/khmer
-
-# 3 ASSEMBLY
-#bash -i MuDoGeR/src/scripts/mudoger-module-1-3_assembly.sh "$master_output_dir"/qc/final_pure_reads_1.fastq "$master_output_dir"/qc/final_pure_reads_2.fastq  "$master_output_dir" "$num_cores"
