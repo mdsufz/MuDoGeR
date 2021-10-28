88aaa83dad86b7fcb1d0c1564700db4bb65b270a
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-10-28 10:44:38 +0200

    Update mudoger.sh

--- src/mudoger.sh
+++ src/mudoger.sh
@@ -85,7 +85,20 @@ if [ "$active_module" = preprocess ]; then
 	#echo $module_script -1 $r1 -2 $r2 -o $output_folder/$i -t $num_cores;
 	#echo 'test'
 	done
-
+	
+if [ "$active_module" = prokaryotes ]; then
+	echo mudoger prokaryotes ${@:2}
+	module_script=MuDoGeR/src/modules/mudoger-module-2.sh
+        ###### loop around samples and run module 1
+	aux="$(while read l ; do echo "$l" | cut -f1; done < "$metadata_table"  | tr '\t' '\n' | sort |  uniq)";
+	for i in $aux; 
+	do 
+	r1="$(cat "$metadata_table" | awk -F '\t' '{ if ($1 == "'$i'") {print} }' | cut -f2 | grep '_1.f')"; 
+	r2="$(cat "$metadata_table" | awk -F '\t' '{ if ($1 == "'$i'") {print} }' | cut -f2 | grep '_2.f')";
+	#ime $module_script -1 $r1 -2 $r2 -o $output_folder/$i -t $num_cores;
+	echo $module_script -1 $r1 -2 $r2 -o $output_folder/$i -t $num_cores;
+	#echo 'test'
+	done
 else
         comm "Please select a proper module of MuDoGeR."
         help_message
