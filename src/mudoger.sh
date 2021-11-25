5725bcbe5dac8c2a1b6dc24269c8e33971dffec3
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-11-25 11:48:49 +0100

    Update mudoger.sh

--- src/mudoger.sh
+++ src/mudoger.sh
@@ -160,14 +160,14 @@ cat */prokaryotes/metrics/GTDBtk_taxonomy/*.sum* | sort | uniq  | grep -v 'user_
 echo -e "bins\tncontigs" > results/prokaryotes/header
 for mag in results/prokaryotes/mags/*;
 do echo -e "$(echo "$mag" | rev | cut -f1 -d'/' | rev)\t\c"; cat $mag | grep ">" | wc -l;
-done > results/prokaryotes/aux_bins_ncontigs.tsv
+done > results/prokaryotes/aux_bins_ncontigs.tsv  
 
 tac results/prokaryotes/aux_bins_ncontigs.tsv > results/prokaryotes/aux2_bins_ncontigs.tsv
-cat  results/prokaryotes/header results/prokaryotes/aux2_bins_ncontigs.tsv > results/prokaryotes/bins_ncontigs.tsv
-
+#cat  results/prokaryotes/header results/prokaryotes/aux2_bins_ncontigs.tsv > results/prokaryotes/bins_ncontigs.tsv
 paste results/prokaryotes/bins_ncontigs.tsv results/prokaryotes/genomic_metrics.tsv > results/prokaryotes/genomic_metrics_aux.tsv
+tac results/prokaryotes/genomic_metrics_aux.tsv > results/prokaryotes/genomic_metrics_aux2.tsv
+cat  results/prokaryotes/header results/prokaryotes/genomic_metrics_aux2.tsv > results/prokaryotes/genomic_metrics.tsv
 
-tac results/prokaryotes/genomic_metrics_aux.tsv > results/prokaryotes/genomic_metrics.tsv
 
 
 #paste results/prokaryotes/bins_ncontigs.tsv
