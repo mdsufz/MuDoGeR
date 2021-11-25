fefb3fe7d71f1bfe4c39eacf997ec5aade52477f
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2021-11-25 12:42:31 +0100

    Update mudoger.sh

--- src/mudoger.sh
+++ src/mudoger.sh
@@ -153,9 +153,9 @@ done > results/prokaryotes/map_otus.tsv
 #ncontigs_header="bins	ncontigs"
 #echo -e "$ncontigs_header" > results/prokaryotes/ncontigs-header
 cat */prokaryotes/metrics/*prok_genomes* | sort | uniq | tac >  results/prokaryotes/genomic_metrics-aux.tsv
-header="$(head -n1 results/prokaryotes/genomic_metrics-aux.tsv)"
+head -n1 results/prokaryotes/genomic_metrics-aux.tsv > results/prokaryotes/ncontigs-header
 cat results/prokaryotes/genomic_metrics-aux.tsv | tail -n +2 >  results/prokaryotes/genomic_metrics-aux2.tsv
-cat -e "$header" results/prokaryotes/genomic_metrics-aux2.tsv > results/prokaryotes/genomic_metrics.tsv
+cat  results/prokaryotes/ncontigs-header results/prokaryotes/genomic_metrics-aux2.tsv > results/prokaryotes/genomic_metrics.tsv
 #cat results/prokaryotes/ncontigs-header results/prokaryotes/genomic_metrics-aux.tsv > results/prokaryotes/genomic_metrics.tsv
 ### checkm
 checkm_header="Bin Id	Marker lineage	# genomes	# markers	# marker sets	0	12	3	4	5+	Completeness	Contamination	Strain heterogeneity"
