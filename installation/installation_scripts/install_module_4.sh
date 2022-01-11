fca97e564bf811b8602fae866d3285526cd01280
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2022-01-11 17:04:52 -0300

    MODULE 4 INSTALLTION TUTORIAL TO BE ADDED

--- installation/installation_scripts/install_module_4.sh
+++ installation/installation_scripts/install_module_4.sh
@@ -15,15 +15,17 @@ mamba install -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/eukrep_env -c bioconda
 ## CREATING GENEMARKER-ES
 conda create -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/genemarker_env
 mamba install -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/genemarker_env -c bioconda perl-local-lib perl-math-utils perl-yaml perl-hash-merge perl-parallel-forkmanager perl-mce perl-threaded
-conda activate $MUDOGER_DEPENDENCIES_ENVS_PATH/genemarker_env
+#FIX HERE !!! ADD REST OF GENEMARK INSTALLATION TUTORIAL
+#conda activate $MUDOGER_DEPENDENCIES_ENVS_PATH/genemarker_env
 mkdir $MUDOGER_DEPENDENCIES_ENVS_PATH/genemarker_env/GENEMARK_MAIN
 
 
 ## CREATING MAKER ENV
 conda create y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/maker2_env
-conda activate $MUDOGER_DEPENDENCIES_ENVS_PATH/maker2_env
-mamba install -y anaconda::perl conda-forge::gcc conda-forge::h5py bioconda::perl-dbi bioconda::perl-module-build bioconda::perl-inline-c bioconda::perl-bit-vector bioconda::perl-io-all bioconda::perl-forks bioconda::perl-dbd-sqlite bioconda::perl-dbd-pg bioconda::perl-perl-unsafe-signals bioconda::perl-want bioconda::perl-bioperl bioconda::perl-bioperl-core bioconda::hmmer bioconda::snap bioconda::augustus bioconda::trf bioconda::blast bioconda::exonerate
+#conda activate $MUDOGER_DEPENDENCIES_ENVS_PATH/maker2_env
+mamba install -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/maker2_env anaconda::perl conda-forge::gcc conda-forge::h5py bioconda::perl-dbi bioconda::perl-module-build bioconda::perl-inline-c bioconda::perl-bit-vector bioconda::perl-io-all bioconda::perl-forks bioconda::perl-dbd-sqlite bioconda::perl-dbd-pg bioconda::perl-perl-unsafe-signals bioconda::perl-want bioconda::perl-bioperl bioconda::perl-bioperl-core bioconda::hmmer bioconda::snap bioconda::augustus bioconda::trf bioconda::blast bioconda::exonerate
 
+#ADD REST OF MAKER 2 TUTORIAL INSTALLATION !!
 
 ## CREATING EUKCC
 conda create  -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/eukcc_env -c bioconda -c biocore hmmer>3.2 pplacer python=3.7
