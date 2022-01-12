ffc239365dc5cd7c6983b4786cbcda708d59840b
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2022-01-12 12:05:37 +0100

    Update install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -59,11 +59,14 @@ cp -r $DEPENDENCIES_SCRIPTS_PATH $MUDOGER_CONDA_ENVIRONMENT_PATH
 #cp -R $INSTALLATION_SCRIPTS_PATH $MUDOGER_DEPENDENCIES_ENVS_PATH # modified by rodolfo
 cp -r $INSTALLATION_SCRIPTS_PATH $MUDOGER_DEPENDENCIES_PATH
 
-
+echo "----"
+core_env="$(echo $PATH | cut -f1 -d':')"
+echo "$core_env"
+echo "----"
+conda activate mudoger_env
 core_env="$(echo $PATH | cut -f1 -d':')"
 echo "----"
 echo "$core_env"
-
 echo 'install line 62 end'
 exit 0
 ################# CHOOSING WICH MODULE TO INSTALL #################
