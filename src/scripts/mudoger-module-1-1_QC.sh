91c8b22bdf377379352d68440db099bbbe0f268a
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2022-01-13 18:00:43 +0100

    Update mudoger-module-1-1_QC.sh

--- src/scripts/mudoger-module-1-1_QC.sh
+++ src/scripts/mudoger-module-1-1_QC.sh
@@ -1,38 +1,26 @@
 #!/bin/bash
 
+################# STARTING MODULE 1-1  ###############################################
+echo '---------- STARTING MODULE 1-1 QC'
+
 # 1 QUALITY CONTROL (QC) OF READS
 # 2 KMER COUNT AND MEMORY ESTIMATION FOR ASSEMBLY
 # 3 ASSEMBLY
-
-
-################# STARTING MODULE 1  ###############################################
-echo '---------- STARTING MODULE 1-1 QC'
-echo $PATH
-
-echo 'close module 1 1 qc line 12'
-exit 0
-
 
 ########## 1 QUALITY CONTROL (QC) OF READS  ###################
 ## load conda metawrap conda quality control
 conda activate mudoger_env
 config_path="$(which config.sh)"
 source $config_path
-
 conda activate "$MUDOGER_DEPENDENCIES_ENVS_PATH"/metawrap_env
 
 # arguments declaration
 log="log_qc"                      # definition of path to QC log       
 forward_library=$1              # forward library path
 reverse_library=$2              # reverse library path
-qc_folder=$3                # output folder to be created inside master output folder
+qc_folder=$3                  # output folder to be created inside master output folder
 bm_tag="--skip-bmtagger"          # define if human reads should be filtered out or not
 num_cores=$4                     # number of threads
-
-
-#out_qc="$( echo "$output_folder"/"$(echo "$forward_library" | rev | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f1 -d'_' )")/qc"          # create output master
-
-#mkdir -p $out_qc
 
 # command of quality control
 metawrap read_qc "$bm_tag" -1 "$forward_library" -2 "$reverse_library" -t "$num_cores" -o "$qc_folder"  &> "$out_qc"/"$log"
