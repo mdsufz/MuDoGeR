99e20a3c38632a76cc5dda96e15fa8f7dbfd050d
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2022-01-28 11:15:36 +0100

    Update mudoger-module-3-2_vcontact2.sh

--- src/scripts/mudoger-module-3-2_vcontact2.sh
+++ src/scripts/mudoger-module-3-2_vcontact2.sh
@@ -42,7 +42,8 @@ fi
 if [ !  -f "$folder_taxonomy"/vcontact-output/genome_by_genome_overview.csv ];
 then
 echo "-> step 3/4"
-vcontact2 -t "$cores" --raw-proteins "$folder_taxonomy"/viral_genomes.faa --rel-mode 'Diamond' --proteins-fp "$folder_taxonomy"/viral_genomes_g2g.csv --db 'ProkaryoticViralRefSeq94-Merged' --pcs-mode MCL --vcs-mode ClusterONE --c1-bin cluster_one-1.0.jar --output-dir "$2"/vcontact-output
+cluster_1_path="$(echo "$(echo $PATH | cut -f1 -d':')"/cluster_one-1.0.jar)"
+vcontact2 -t "$cores" --raw-proteins "$folder_taxonomy"/viral_genomes.faa --rel-mode 'Diamond' --proteins-fp "$folder_taxonomy"/viral_genomes_g2g.csv --db 'ProkaryoticViralRefSeq94-Merged' --pcs-mode MCL --vcs-mode ClusterONE --c1-bin "$cluster_1_path" --output-dir "$2"/vcontact-output
 else :
 fi
 
