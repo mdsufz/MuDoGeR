e727e112b280545188f8c7e3f980a37380033f89
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2022-02-07 16:09:18 +0100

    Update mudoger-module-4-1_eukrep-eukbin-filter.sh

--- src/scripts/mudoger-module-4-1_eukrep-eukbin-filter.sh
+++ src/scripts/mudoger-module-4-1_eukrep-eukbin-filter.sh
@@ -1,7 +1,17 @@
-
 #!/bin/bash
 
 ########## 1 INITIAL PROKARYOTIC BINNING  ###################
+
+# fetch eukaryotic contigs/scaffolds
+# loading conda environment
+conda activate mudoger_env
+config_path="$(which config.sh)"
+database="${config_path/config/database}"
+source $config_path
+source $database
+
+conda activate "$MUDOGER_DEPENDENCIES_ENVS_PATH"/eukrep_env
+
 
 # arguments declaration    
 assembly=$1                     # assembly fasta file
@@ -12,14 +22,13 @@ num_cores=$5                    # number of threads
 memory=$6
 
 
-# fetch eukaryotic contigs/scaffolds
-conda activate eukrep-env
+
 EukRep -i "$assembly" --prokarya "$output_folder"/prokaryotic_contigs.fa -o "$output_folder"/eukaryotic_contigs.fa
 conda deactivate
 
 
 # run concoct binnning
-conda activate metawrap-env
+conda activate "$MUDOGER_DEPENDENCIES_ENVS_PATH"/metawrap_env
 metawrap binning -o "$output_folder" -t "$num_cores" -a "$output_folder"/eukaryotic_contigs.fa --concoct "$forward_library" "$reverse_library" # -m "$memory"
 
 
