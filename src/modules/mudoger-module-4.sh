85eb7cd11403282fa11e7c8eace6c5afdd9414cc
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2022-02-07 15:57:59 +0100

    Update mudoger-module-4.sh

--- src/modules/mudoger-module-4.sh
+++ src/modules/mudoger-module-4.sh
@@ -13,23 +13,31 @@
 #       N50, NUM_NUCLEOTIDE, NUM_CONTIGS, ATCG and more... submodule 3-5
 
 
+# loop through input params
+while true; do
+	case "$1" in
+		-1) forward_library=$2; shift 2;;
+		-2) reverse_library=$2; shift 2;;
+		-a) assembly=$2; shift 2;;
+		-o) libname_folder=$2; shift 2;;
+		-t) cores=$2; shift 2;;
+		-m) memory=$2; shift 2;;
+		-h | --help) help_message; exit 1; shift 1;;
+		--) help_message; exit 1; shift; break ;;
+		*) break;;
+	esac
+done
 
-libname=$1
-assembly=$2
-forward_library=$3            # forward library path /path/to/libname_1.fastq
-reverse_library=$4            # reverse library path. /path/to/libname_2.fastq
-output_folder=$5              # master output folder to be created and defined by user
-cores=1
 
 # 1 EUKREP CONTIG FILTERING AND  BINNING USING CONCOCT
 
 mkdir -p "$libname_folder"/eukaryotes/eukaryotic_contigs
-#bash -i MuDoGeR/src/scripts/mudoger-module-4-1_eukrep-eukbin-filter.sh "$assembly"       \
-#                                      "$forward_library"                                 \
-#                                      "$reverse_library"                                 \
-#                                      "$libname_folder"/eukaryotes			 \
-#                                      "$cores"                                           \  
-#				      "$memory"
+bash -i $MUDOGER_CONDA_ENVIRONMENT_PATH/bin/mudoger-module-4-1_eukrep-eukbin-filter.sh "$assembly"       \
+                                      "$forward_library"                                 \
+                                      "$reverse_library"                                 \
+                                      "$libname_folder"/eukaryotes			 \
+                                      "$cores"                                           \  
+				      "$memory"
 
 bash -i MuDoGeR/src/scripts/mudoger-module-4-2_genemark-maker-busco.sh "$assembly"       \
                                       "$forward_library"                                 \
