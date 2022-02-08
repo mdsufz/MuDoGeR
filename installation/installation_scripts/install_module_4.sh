813b351fea2af74ef7de1c030af0a1db7e38b18d
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2022-02-08 10:20:00 +0100

    Update install_module_4.sh

--- installation/installation_scripts/install_module_4.sh
+++ installation/installation_scripts/install_module_4.sh
@@ -9,13 +9,7 @@ source installation/config.sh              # modified by rodolfo
 source installation/installation_utils.sh  # modified by rodolfo
 
 ## CREATING EUKREP ENVIRONMENT
-conda create -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/eukrep_env
-mamba install -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/eukrep_env pip
-conda activate $MUDOGER_DEPENDENCIES_ENVS_PATH/eukrep_env
-pip install --upgrade setuptools
-mamba install -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/eukrep_env -c bioconda eukrep
-mamba install -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/eukrep_env -c bioconda scikit-learn==0.19.2 
-conda deactivate
+conda create -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/eukrep_env -c bioconda  scikit-learn==0.19.2 eukrep
 
 ## CREATING GENEMARKER-ES
 conda create -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/genemarker_env
