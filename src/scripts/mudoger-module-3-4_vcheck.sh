d6100e6f4759712dda444d4bf185c525aa3f27cb
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2022-02-10 15:20:57 +0100

    Update mudoger-module-3-4_vcheck.sh

--- src/scripts/mudoger-module-3-4_vcheck.sh
+++ src/scripts/mudoger-module-3-4_vcheck.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+conda activate mudoger_env
+config_path="$(which config.sh)"
+database="${config_path/config/database}"
+source $config_path
+source $database
+
 
 input_uvigs="$1"
 output_checkv="$2"
 cores="$3"
 
 
-conda activate checkv-env
+
+conda activate "$MUDOGER_DEPENDENCIES_ENVS_PATH"/checkv_env
+
+
 export CHECKVDB=/mnt/databases/checkv/checkv-db-v1.0
 
 
 checkv end_to_end  "$input_uvigs" "$output_checkv" -t "$cores"
+~                                                                                                                 
+~                                                                                                                 
+~                                                                                                                 
+~                                                                                                                 
+~                                                                                                                 
+~                                                                                   
