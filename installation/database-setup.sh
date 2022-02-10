c6659914ca11ffd3571c636882467eafe85e0e38
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2022-02-10 15:53:50 +0100

    Update database-setup.sh

--- installation/database-setup.sh
+++ installation/database-setup.sh
@@ -12,37 +12,39 @@ source "$config_file"
 echo DATABASES_LOCATION="$database_location" > ${config_file/config/database}
 
 
+############################################### PROKARYOTES ###############################################
+### CheckM
+mkdir -p "$database_location"/checkm
+cd  "$database_location"/checkm
+if [ ! -f selected_marker_sets.tsv ]; then
+echo 'installing checkm database ...'
+wget https://data.ace.uq.edu.au/public/CheckM_databases/checkm_data_2015_01_16.tar.gz
+tar -xvf checkm_data_2015_01_16.tar.gz
+rm -fr checkm_data_2015_01_16.tar.gz
+# On newer versions of CheckM, you would run:
+#checkm data setRoot /path/to/your/dir/$MY_CHECKM_FOLDER
+CHECKM_DB="$database_location"/checkm #Fixed? we need to test
+echo CHECKM_DB="$CHECKM_DB" >> "$config_path"
+else echo "-> your CheckM database is ready"
+fi
+
+
+### GTDB-tk
+mkdir -p  "$database_location"/"gtdbtk"
+cd "$database_location"/"gtdbtk"
+if [ ! -d release*  ]; then
+wget https://data.gtdb.ecogenomic.org/releases/latest/auxillary_files/gtdbtk_data.tar.gz
+tar xvzf gtdbtk_data.tar.gz
+rm -fr gtdbtk_data.tar.gz
+#echo  GTDBTK_DATA_PATH="$database_location"/gtdbtk/gtdbtk_r95_data >> "$config_file" ##FIX HERE
+#echo GTDBTK_DATA_PATH="$(ls "$database_location"/gtdbtk/release*)" >>  "$config_file" # fixed? we need to test
+else echo "-> your GTDBtk database is ready"
+fi
 
 
 
-########################################################## WISH
-
-WISH_DB_DIR=$database_location/wish
-mkdir -p $WISH_DB_DIR
-wget "https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.1.1.genomic.fna.gz" -P $WISH_DB_DIR
-wget "https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.2.1.genomic.fna.gz" -P $WISH_DB_DIR
-wget "https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.3.1.genomic.fna.gz" -P $WISH_DB_DIR
-wget "https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.4.1.genomic.fna.gz" -P $WISH_DB_DIR
-
-gunzip $WISH_DB_DIR/*
-cat $WISH_DB_DIR/* > $WISH_DB_DIR/viral_refseq.fna
-python3 $MUDOGER_DEPENDENCIES_PATH/split-all-seq.py $WISH_DB_DIR/viral_refseq.fna $WISH_DB_DIR/viruses
-
-for d in $WISH_DB_DIR/viruses-*fa; 
-do 
-    if grep -q phage "$d"; 
-    then :; 
-else 
-    rm -f "$d"; 
-fi; 
-done
-
-mv $WISH_DB_DIR/viruses* $WISH_DB_DIR/phages
-rm -rf $WISH_DB_DIR/viral.1.1.genomic.fna  $WISH_DB_DIR/viral.2.1.genomic.fna  $WISH_DB_DIR/viral.3.1.genomic.fna  $WISH_DB_DIR/viral.4.1.genomic.fna $WISH_DB_DIR/viral_refseq.fna
-
-
-
-########################################################## VIBRANT
+############################################### VIRUSES ###############################################
+## VIBRANT
 conda activate $MUDOGER_DEPENDENCIES_ENVS_PATH/vibrant_env
 pip install pickle-mixin --quiet
 VIBRANT_DB_DIR=$database_location/vibrant
@@ -95,37 +97,37 @@ echo '-> your VIBRANT database is now ready'
 else echo '-> your VIBRANT database is ready'
 fi
 
+# WISH
+WISH_DB_DIR=$database_location/wish
+if [ ! -f  $database_location/.wish_db_finished ];
+then
+mkdir -p $WISH_DB_DIR
+wget "https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.1.1.genomic.fna.gz" -P $WISH_DB_DIR
+wget "https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.2.1.genomic.fna.gz" -P $WISH_DB_DIR
+wget "https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.3.1.genomic.fna.gz" -P $WISH_DB_DIR
+wget "https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.4.1.genomic.fna.gz" -P $WISH_DB_DIR
 
-############################################### PROKARYOTES ###############################################
-### CheckM
-mkdir -p "$database_location"/checkm
-cd  "$database_location"/checkm
-if [ ! -f selected_marker_sets.tsv ]; then
-echo 'installing checkm database ...'
-wget https://data.ace.uq.edu.au/public/CheckM_databases/checkm_data_2015_01_16.tar.gz
-tar -xvf checkm_data_2015_01_16.tar.gz
-rm -fr checkm_data_2015_01_16.tar.gz
-# On newer versions of CheckM, you would run:
-#checkm data setRoot /path/to/your/dir/$MY_CHECKM_FOLDER
-CHECKM_DB="$database_location"/checkm #Fixed? we need to test
-echo CHECKM_DB="$CHECKM_DB" >> "$config_path"
-else echo "-> your CheckM database is ready"
+gunzip $WISH_DB_DIR/*
+cat $WISH_DB_DIR/* > $WISH_DB_DIR/viral_refseq.fna
+python3 $MUDOGER_DEPENDENCIES_PATH/split-all-seq.py $WISH_DB_DIR/viral_refseq.fna $WISH_DB_DIR/viruses
+
+for d in $WISH_DB_DIR/viruses-*fa;
+do
+    if grep -q phage "$d";
+    then :;
+else
+    rm -f "$d";
+fi;
+done
+
+mv $WISH_DB_DIR/viruses* $WISH_DB_DIR/phages
+rm -rf $WISH_DB_DIR/viral.1.1.genomic.fna  $WISH_DB_DIR/viral.2.1.genomic.fna  $WISH_DB_DIR/viral.3.1.genomic.fna  $WISH_DB_DIR/viral.4.1.genomic.fna $WISH_DB_DIR/viral_refseq.fna
+touch $database_location/.wish_db_finished
+else
+echo "-> your Wish database is ready"
 fi
 
 
-### GTDB-tk
-mkdir -p  "$database_location"/"gtdbtk"
-cd "$database_location"/"gtdbtk"
-if [ ! -f gtdbtk_data.tar.gz  ]; then
-wget https://data.gtdb.ecogenomic.org/releases/latest/auxillary_files/gtdbtk_data.tar.gz
-tar xvzf gtdbtk_data.tar.gz
-rm -fr gtdbtk_data.tar.gz
-#echo  GTDBTK_DATA_PATH="$database_location"/gtdbtk/gtdbtk_r95_data >> "$config_file" ##FIX HERE
-echo GTDBTK_DATA_PATH="$(ls "$database_location"/gtdbtk/release*)" >>  "$config_file" # fixed? we need to test
-else echo "-> your GTDBtk database is ready"
-fi
-
-############################################### VIRUSES ###############################################
 ### CheckV
 mkdir -p  "$database_location"/checkv
 cd "$database_location"/checkv
@@ -134,10 +136,11 @@ wget https://portal.nersc.gov/CheckV/checkv-db-v1.0.tar.gz
 tar -zxvf checkv-db-v1.0.tar.gz
 rm -fr checkv-db-v1.0.tar.gz
 #ADD CHECKV DATABASE PATH TO CONFIG FILE
-CHECKVDB="$database_location"/checkv/checkv-db-v1.0
-echo CHECKVDB="$CHECKVDB" >> "$config_file"
+#CHECKVDB="$database_location"/checkv/checkv-db-v1.0
+#echo CHECKVDB="$CHECKVDB" >> "$config_file"
 else echo "-> your CheckV database is ready"
 fi
+
 
 ############################################### EUKARYOTES ###############################################
 
