7d98c200f2bdde1976d770c20bb9779c5c8f4518
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2022-03-17 14:19:44 -0300

    Clean

--- src/scripts/mudoger-module-4-1_eukrep-eukbin-filter.sh
+++ src/scripts/mudoger-module-4-1_eukrep-eukbin-filter.sh
@@ -10,7 +10,6 @@ database="${config_path/config/database}"
 source $config_path
 source $database
 
-conda activate "$MUDOGER_DEPENDENCIES_ENVS_PATH"/eukrep_env
 
 
 # arguments declaration    
@@ -21,25 +20,32 @@ output_folder=$4                # output folder to be created inside master outp
 num_cores=$5                    # number of threads
 memory=$6
 
+#run eukrep
+conda activate "$MUDOGER_DEPENDENCIES_ENVS_PATH"/eukrep_env
+mkdir -p "$output_folder"/eukaryotes_contigs
+EukRep -i "$assembly" --prokarya "$output_folder"/prokaryotic_contigs.fa -o "$output_folder"/eukaryotes_contigs/eukaryotic_contigs.fa
 
-
-EukRep -i "$assembly" --prokarya "$output_folder"/prokaryotic_contigs.fa -o "$output_folder"/eukaryotic_contigs.fa
 conda deactivate
-
+#End eukrep
 
 # run concoct binnning
 conda activate "$MUDOGER_DEPENDENCIES_ENVS_PATH"/metawrap_env
-metawrap binning -o "$output_folder" -t "$num_cores" -a "$output_folder"/eukaryotic_contigs.fa --concoct "$forward_library" "$reverse_library" # -m "$memory"
+mkdir -p "$output_folder"/eukaryotes_bins
+metawrap binning -o "$output_folder"/eukaryotes_bins -t "$num_cores" -a "$output_folder"/eukaryotic_contigs.fa --concoct "$forward_library" "$reverse_library" 
 
+conda deactivate
+#End CONCOCT binning
 
-# keep bins bigger than 2,5mb
-mkdir "$output_folder"/filtered_bin_size
-for bin in "$output_folder"/concoct_bins/*fa;
+#Start Filtering
+# keep bins bigger than 2,0mb
+mkdir -p "$output_folder"/filtered_euk_bins
+for bin in "$output_folder"/eukaryotes_bins/*fa;
 do  size="$(stat $bin | grep Size | cut -f1  | cut -f4 -d' ' )";
-if [ $size -gt 2500000 ]; then  cp "$bin" "$output_folder"/filtered_bin_size ;
+if [ $size -gt 2000000 ]; then  cp "$bin" "$output_folder"/filtered_euk_bins ;
 else : ; fi ;
 done
 
+#End filtering
 
 
 
