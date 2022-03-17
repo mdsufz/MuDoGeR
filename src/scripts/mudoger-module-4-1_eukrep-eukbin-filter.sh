58640268bc683d1afbeb59ab861e8c7bca92bb92
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2022-03-17 14:34:28 -0300

    Update mudoger-module-4-1_eukrep-eukbin-filter.sh

--- src/scripts/mudoger-module-4-1_eukrep-eukbin-filter.sh
+++ src/scripts/mudoger-module-4-1_eukrep-eukbin-filter.sh
@@ -21,22 +21,27 @@ num_cores=$5                    # number of threads
 memory=$6
 
 #run eukrep
+echo -e "\n --->RUNNING EUKREP"
 conda activate "$MUDOGER_DEPENDENCIES_ENVS_PATH"/eukrep_env
 mkdir -p "$output_folder"/eukaryotes_contigs
 EukRep -i "$assembly" --prokarya "$output_folder"/prokaryotic_contigs.fa -o "$output_folder"/eukaryotes_contigs/eukaryotic_contigs.fa
 
 conda deactivate
+echo -e "\n --->END EUKREP"
 #End eukrep
 
 # run concoct binnning
+echo -e "\n --->RUNNING CONCOCT BINNING"
 conda activate "$MUDOGER_DEPENDENCIES_ENVS_PATH"/metawrap_env
 mkdir -p "$output_folder"/eukaryotes_bins
 metawrap binning -o "$output_folder"/eukaryotes_bins -t "$num_cores" -a "$output_folder"/eukaryotic_contigs.fa --concoct "$forward_library" "$reverse_library" 
 
 conda deactivate
+echo -e "\n --->END CONCOCT BINNING"
 #End CONCOCT binning
 
 #Start Filtering
+echo -e "\n --->RUNNING FILTER"
 # keep bins bigger than 2,0mb
 mkdir -p "$output_folder"/filtered_euk_bins
 for bin in "$output_folder"/eukaryotes_bins/*fa;
@@ -44,7 +49,7 @@ do  size="$(stat $bin | grep Size | cut -f1  | cut -f4 -d' ' )";
 if [ $size -gt 2000000 ]; then  cp "$bin" "$output_folder"/filtered_euk_bins ;
 else : ; fi ;
 done
-
+echo -e "\n --->END FILTER"
 #End filtering
 
 
