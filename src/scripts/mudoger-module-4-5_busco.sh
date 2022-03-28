0941476080b8ba8a7c77e3d4b50174b029fc6f07
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2022-03-28 13:29:55 -0300

    Added busco

--- src/scripts/mudoger-module-4-5_busco.sh
+++ src/scripts/mudoger-module-4-5_busco.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-########## RUNNING EUKCC ###################
+########## RUNNING BUSCO ###################
 
 # loading conda environment and adjusting the paths
 conda activate mudoger_env
@@ -9,43 +9,43 @@ database="${config_path/config/database}"
 source $config_path
 source $database
 
-#Set eukcc database location
-eukcc_db_path="$DATABASES_LOCATION/eukccdb"
+#Set busco database location
+busco_db_path="$DATABASES_LOCATION/buscodbs"
 
 
 # arguments declaration    
 euk_folder=$1 #"$libname_folder"/eukaryotes
 
-filtered_euk_bins_folder="$1/filtered_euk_bins"     #folder containing the filtered eukaryotic bins
-output_folder="$1/eukcc_quality"            #output folder
+maker2_genes_folder="$1/maker2_gene_annotation"     #folder containing the euk_bin with any annotated maker2 genes
+output_folder="$1/euk_completeness"                 #output folder
 
 num_core=$2
 
 
-# Run EUKCC
+# Run BUSCO
 
-echo -e "\n --->RUNNING EUKCC"
+echo -e "\n --->RUNNING BUSCO"
 conda activate "$MUDOGER_DEPENDENCIES_ENVS_PATH"/busco_env
 mkdir -p $output_folder
 
-for bin_path in $filtered_euk_bins_folder/*; do
+for bin_path in $maker2_genes_folder/*; do
 
-bin=`echo ${bin_path} | rev | cut -f1 -d'/' | rev`
+bin=`${bin_path} | rev | cut -f1 -d'/' | cut -d '.' -f2- | rev`
 
-if [ -f  $output_folder/"$bin"_eukcc/eukcc.csv ];
+if [ -f  $output_folder/"$bin"_busco/run_lineage_name/full_table.tsv ];
 then
 :
 else
 
-mkdir -p $output_folder/"$bin"_eukcc
+#mkdir -p $output_folder/"$bin"_busco/
+sequence_path=$bin_path/"$bin".maker.output/OUTPUT.all.maker.genemark.proteins.fasta
 
-
-eukcc single --db $eukcc_db_path --threads $num_core --out $output_folder/"$bin"_eukcc $bin_path
+busco -i $sequence_path -l "$busco_db_path"/lineages/eukaryota_odb10 -m protein -o $output_folder/"$bin"_busco/ -c $num_core
 
 fi
 done
 conda deactivate
 
-echo -e "\n --->END EUKCC QUALITY CALCULATION"
+echo -e "\n --->END BUSCO CALCULATION"
 
-#End EUKCC
+#End BUSCO
