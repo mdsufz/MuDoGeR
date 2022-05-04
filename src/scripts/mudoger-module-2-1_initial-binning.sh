00551f1d34a240da2b1e2cef3a08cae8a63363a8
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2022-05-04 15:05:44 -0300

    clean

--- src/scripts/mudoger-module-2-1_initial-binning.sh
+++ src/scripts/mudoger-module-2-1_initial-binning.sh
@@ -6,6 +6,7 @@ echo '------- START MODULE 2-1 INITIAL BINNING'
 conda activate mudoger_env
 config_path="$(which config.sh)"
 source $config_path
+
 conda activate "$MUDOGER_DEPENDENCIES_ENVS_PATH"/metawrap_env
 
 # arguments declaration    
@@ -16,13 +17,5 @@ output_folder=$4                # output folder to be created inside master outp
 num_cores=$5                    # number of threads
 memory=$6
 
-#mkdir $prok/bact_ref
-#outb="$prok/bact_ref"
-#mkdir $prok/arch_ref
-#outa="$prok/arch_ref"
-
-#con="$prok/concoct_bins"
-#met="$prok/metabat2_bins"
-#max="$prok/maxbin2_bins"
 
 metawrap binning -o "$output_folder" -t "$num_cores" -a "$assembly" --metabat2 --maxbin2 --concoct "$forward_library" "$reverse_library" # -m "$memory" 
