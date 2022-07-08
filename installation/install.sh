0f5679ae12d9d64a916d22344acac79fb1745a5c
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2022-07-08 16:48:04 +0200

    Update install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -21,7 +21,7 @@
 
 conda activate base
 echo "---> installing mamba"
-conda install -c conda-forge mamba
+conda install -y -c conda-forge mamba
 MUDOGER_CONDA_ENVIRONMENT_PATH=$CONDA_PREFIX/envs/mudoger_env
 echo MUDOGER_CONDA_ENVIRONMENT_PATH="$MUDOGER_CONDA_ENVIRONMENT_PATH" > $(dirname $0)/temp
 cat $(dirname $0)/temp  $(dirname $0)/.config_std.sh > $(dirname $0)/config.sh
