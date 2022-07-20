3f289368c3a7ca14a12c8f3e963766d806c26488
Jonas C K <54112176+JotaKas@users.noreply.github.com>
2022-07-20 11:39:34 +0200

    Update install_module_4.sh

--- installation/installation_scripts/install_module_4.sh
+++ installation/installation_scripts/install_module_4.sh
@@ -46,9 +46,9 @@ verify_if_conda_env_exist maker2_env
 if [ $PRESENT == 'yes' ]
 then :;
 else
-conda create -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/maker2_env
+conda create -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/maker2_env anaconda::perl conda-forge::gcc conda-forge::h5py bioconda::perl-dbi bioconda::perl-module-build bioconda::perl-inline-c bioconda::perl-bit-vector bioconda::perl-io-all bioconda::perl-forks bioconda::perl-dbd-sqlite bioconda::perl-dbd-pg bioconda::perl-perl-unsafe-signals bioconda::perl-want bioconda::perl-bioperl bioconda::perl-bioperl-core bioconda::hmmer bioconda::snap bioconda::augustus bioconda::trf bioconda::blast bioconda::exonerate
 #mamba has problems with perl
-conda install -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/maker2_env anaconda::perl conda-forge::gcc conda-forge::h5py bioconda::perl-dbi bioconda::perl-module-build bioconda::perl-inline-c bioconda::perl-bit-vector bioconda::perl-io-all bioconda::perl-forks bioconda::perl-dbd-sqlite bioconda::perl-dbd-pg bioconda::perl-perl-unsafe-signals bioconda::perl-want bioconda::perl-bioperl bioconda::perl-bioperl-core bioconda::hmmer bioconda::snap bioconda::augustus bioconda::trf bioconda::blast bioconda::exonerate
+#conda install -y --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/maker2_env anaconda::perl conda-forge::gcc conda-forge::h5py bioconda::perl-dbi bioconda::perl-module-build bioconda::perl-inline-c bioconda::perl-bit-vector bioconda::perl-io-all bioconda::perl-forks bioconda::perl-dbd-sqlite bioconda::perl-dbd-pg bioconda::perl-perl-unsafe-signals bioconda::perl-want bioconda::perl-bioperl bioconda::perl-bioperl-core bioconda::hmmer bioconda::snap bioconda::augustus bioconda::trf bioconda::blast bioconda::exonerate
 conda activate $MUDOGER_DEPENDENCIES_ENVS_PATH/maker2_env
 cpanm List::MoreUtils
 mkdir $MUDOGER_DEPENDENCIES_ENVS_PATH/maker2_env/MAKER_MAIN
