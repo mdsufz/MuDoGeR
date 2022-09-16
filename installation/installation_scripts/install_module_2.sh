008e3ebf233c12c2eb383e0e6dbfa492304b2aca
Jonas C Kasmanas <54112176+JotaKas@users.noreply.github.com>
2022-09-16 10:44:34 +0200

    Update install_module_2.sh

--- installation/installation_scripts/install_module_2.sh
+++ installation/installation_scripts/install_module_2.sh
@@ -39,7 +39,7 @@ cp -r $MUDOGER_CLONED_TOOLS_PATH/metaWRAP/bin/* $MUDOGER_DEPENDENCIES_ENVS_PATH/
 
 echo '----> mamba install metawrap'
 mamba install --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/metawrap_env -y --only-deps -c ursky metawrap-mg
-conda install -c bioconda samtools=1.9 --force-reinstall
+conda install --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/metawrap_env -y -c bioconda samtools=1.9 --force-reinstall
 mamba install --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/metawrap_env -y -c bioconda bwa quast megahit trim-galore fastqc metabat2
 mamba update --prefix $MUDOGER_DEPENDENCIES_ENVS_PATH/metawrap_env -y spades
 echo '----> done'
