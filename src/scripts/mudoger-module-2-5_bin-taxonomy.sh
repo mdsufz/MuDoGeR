acb57df8c25329bab81ee48d5c320b5ebeebec23
Jonas C Kasmanas <54112176+JotaKas@users.noreply.github.com>
2023-01-25 16:04:01 +0100

    create merged gtdb results

--- src/scripts/mudoger-module-2-5_bin-taxonomy.sh
+++ src/scripts/mudoger-module-2-5_bin-taxonomy.sh
@@ -18,7 +18,19 @@ export GTDBTK_DATA_PATH=$GTDBTK_DATA_PATH
 master_folder=$1
 cores=$2
 
-#Run 
+#Run
+if [ -f "$master_folder"/metrics/GTDBtk_taxonomy/gtdbtk.bac120.summary.tsv ] || [ -f "$master_folder"/metrics/GTDBtk_taxonomy/gtdbtk.ar53.summary.tsv ];
+then echo "";
+else
 gtdbtk classify_wf --extension fa --cpus "$cores" --genome_dir "$master_folder"/binning/unique_bins --out_dir "$master_folder"/metrics/GTDBtk_taxonomy
+fi
+
+#Create merged results file
+if [ -f "$master_folder"/metrics/GTDBtk_taxonomy/gtdbtk.bac120.summary.tsv ] || [ -f "$master_folder"/metrics/GTDBtk_taxonomy/gtdbtk.ar53.summary.tsv ]; 
+then awk 'FNR==1 && NR!=1 {next;}{print}' "$master_folder"/metrics/GTDBtk_taxonomy/gtdbtk.*summ*.tsv > "$master_folder"/metrics/GTDBtk_taxonomy/gtdbtk_result.tsv;
+echo "GTDBtk results generated!"
+else
+echo "Error: GTDBtk summary files not found"
+fi
 
 conda deactivate
