a6b1c1f9d1e76f8bbaefafcacaf83b2c5ca12303
Jonas C Kasmanas <54112176+JotaKas@users.noreply.github.com>
2023-08-08 10:05:39 -0300

    Update GTDBtk

--- installation/database-setup.sh
+++ installation/database-setup.sh
@@ -66,8 +66,14 @@ if [ "$active_module" = "all" ]; then
     #https://data.gtdb.ecogenomic.org/releases/latest/auxillary_files/gtdbtk_data.tar.gz
     #wget https://data.gtdb.ecogenomic.org/releases/latest/auxillary_files/gtdbtk_v2_data.tar.gz
     wget https://data.gtdb.ecogenomic.org/releases/latest/auxillary_files/gtdbtk_data.tar.gz
-    tar xzf gtdbtk_data.tar.gz
-    rm -fr gtdbtk_data.tar.gz
+    #tar xzf gtdbtk_data.tar.gz
+    #rm -fr gtdbtk_data.tar.gz
+
+    # Extract the contents (assuming a tar archive)
+    tar xzf gtdbtk_data.*
+
+    # Remove the downloaded file
+    rm -fr gtdbtk_data.*
 
     else echo "-> your GTDBtk database is ready"
     fi
@@ -220,9 +226,17 @@ elif [ "$active_module" = "prokaryotes" ]; then
     cd "$database_location"/"gtdbtk"
     if [ ! -d release*  ]; then
     #https://data.gtdb.ecogenomic.org/releases/latest/auxillary_files/gtdbtk_data.tar.gz
-    wget https://data.gtdb.ecogenomic.org/releases/latest/auxillary_files/gtdbtk_v2_data.tar.gz
-    tar -xzf gtdbtk_v2_data.tar.gz
-    rm -fr gtdbtk_v2_data.tar.gz
+    #wget https://data.gtdb.ecogenomic.org/releases/latest/auxillary_files/gtdbtk_v2_data.tar.gz
+    #tar -xzf gtdbtk_v2_data.tar.gz
+    #rm -fr gtdbtk_v2_data.tar.gz
+    
+    wget https://data.gtdb.ecogenomic.org/releases/latest/auxillary_files/gtdbtk_data.tar.gz
+    # Extract the contents (assuming a tar archive)
+    tar xzf gtdbtk_data.*
+
+    # Remove the downloaded file
+    rm -fr gtdbtk_data.*
+
 
     else echo "-> your GTDBtk database is ready"
     fi
